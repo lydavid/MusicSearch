785deae58fed443707d5442a13f209604baefc22
David Ly <d.ly@alum.utoronto.ca>
2022-02-19 13:37:05 -0500

    Change nullability of Track based on SQL table's nullability

--- app/src/main/java/ly/david/musicbrainzjetpackcompose/data/Track.kt
+++ app/src/main/java/ly/david/musicbrainzjetpackcompose/data/Track.kt
@@ -4,12 +4,13 @@ import com.squareup.moshi.Json
 
 data class Track(
     @Json(name = "id") val id: String,
-    @Json(name = "position") val position: Int,
-    @Json(name = "number") val number: String? = null, // Seems to be a string of position
-    @Json(name = "length") val length: Int,
-    @Json(name = "title") val title: String? = null,
-    @Json(name = "artist-credit") val artistCredit: List<ArtistCredit>? = null,
     @Json(name = "recording") val recording: Recording,
+    @Json(name = "position") val position: Int,
+    @Json(name = "number") val number: String, // Seems to be a string of `position`
+    @Json(name = "title") val title: String? = null, // From recording? or is the `name` field
+    @Json(name = "artist-credit") val artistCredit: List<ArtistCredit>? = null,
+    @Json(name = "length") val length: Int? = null,
+
     @Json(name = "relations") val relations: List<Relation>? = null,
 )
 
