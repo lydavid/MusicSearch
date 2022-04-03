2a71a4c3c2236536c391c534882182a55fc28073
David Ly <d.ly@alum.utoronto.ca>
2022-04-03 14:17:43 -0400

    Remove sortAndGroupByTypes

--- app/src/main/java/ly/david/mbjc/data/ReleaseGroup.kt
+++ app/src/main/java/ly/david/mbjc/data/ReleaseGroup.kt
@@ -1,11 +1,9 @@
 package ly.david.mbjc.data
 
-import ly.david.mbjc.data.domain.UiReleaseGroup
-
 /**
  * Defines common properties between domain, network and persistence model.
  */
-interface ReleaseGroup: NameWithDisambiguation, ReleaseGroupTypes {
+interface ReleaseGroup : NameWithDisambiguation, ReleaseGroupTypes {
 
     val id: String
     override val name: String
@@ -15,23 +13,3 @@ interface ReleaseGroup: NameWithDisambiguation, ReleaseGroupTypes {
     override val primaryType: String?
     override val secondaryTypes: List<String>?
 }
-
-// TODO: ordering actually has null first. Right now, that would push bootlegs to the top, so we're not doing it.
-private val primaryPrecedence = listOf("Album", "Single", "EP", "Broadcast", "Other")
-
-// TODO: Album + Compilation would be followed by Album + Compilation + Live + Remix, etc
-private val secondaryPrecedence = listOf(
-    listOf(), listOf("Compilation")
-)
-
-private fun Int.moveNotFoundToEnd() = if (this == -1) Int.MAX_VALUE else this
-
-fun List<UiReleaseGroup>.sortAndGroupByTypes(): Map<String, List<UiReleaseGroup>> =
-    this.sortedWith(
-        compareBy<UiReleaseGroup> {
-            primaryPrecedence.indexOf(it.primaryType).moveNotFoundToEnd()
-        }.thenBy {
-            secondaryPrecedence.indexOf(it.secondaryTypes).moveNotFoundToEnd()
-        }
-    )
-        .groupBy { it.getDisplayTypes() }
