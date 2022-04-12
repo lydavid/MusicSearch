52a6c32cc6eff62e06ff80e7528b08333a633481
David Ly <d.ly@alum.utoronto.ca>
2022-04-11 21:55:43 -0400

    More tests

--- app/src/androidTest/java/ly/david/mbjc/data/persistence/ReleaseGroupDaoTest.kt
+++ app/src/androidTest/java/ly/david/mbjc/data/persistence/ReleaseGroupDaoTest.kt
@@ -78,7 +78,7 @@ internal class ReleaseGroupDaoTest {
                 listOf(
                     ReleaseGroupRoomModel(
                         id = "rg1",
-                        name = "Should fine me"
+                        name = "Should find me"
                     ),
                     ReleaseGroupRoomModel(
                         id = "rg2",
@@ -109,14 +109,12 @@ internal class ReleaseGroupDaoTest {
                 )
             )
 
-            val pagingSource = releaseGroupDao.getReleaseGroupsByArtist("a1")
-            val loadResult: PagingSource.LoadResult<Int, ReleaseGroupRoomModel> = pagingSource.load(
-                PagingSource.LoadParams.Refresh(key = null, loadSize = SEARCH_BROWSE_LIMIT, placeholdersEnabled = false)
+            val foundReleaseGroups = getReleaseGroupsFromPagingSource(
+                releaseGroupDao.getReleaseGroupsByArtist("a1")
             )
-            val foundReleaseGroups = (loadResult as PagingSource.LoadResult.Page<Int, ReleaseGroupRoomModel>).data
 
             assertEquals(
-                "Should fine me",
+                "Should find me",
                 foundReleaseGroups[0].name
             )
             assertEquals(
@@ -124,5 +122,142 @@ internal class ReleaseGroupDaoTest {
                 foundReleaseGroups[1].name
             )
         }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getReleaseGroupsByArtistSortedByPrimaryType() {
+        runBlocking {
+            artistDao.insert(
+                ArtistRoomModel(
+                    id = "a1",
+                    name = "Artist"
+                )
+            )
+            releaseGroupDao.insertAll(
+                listOf(
+                    ReleaseGroupRoomModel(
+                        id = "rg1",
+                        name = "Should find me",
+                        primaryType = "Single"
+                    ),
+                    ReleaseGroupRoomModel(
+                        id = "rg2",
+                        name = "and me",
+                        primaryType = "Album"
+                    ),
+                    ReleaseGroupRoomModel(
+                        id = "rg3",
+                        name = "but not me"
+                    ),
+                )
+            )
+            releaseGroupArtistDao.insertAll(
+                listOf(
+                    ReleaseGroupArtistCreditRoomModel(
+                        releaseGroupId = "rg1",
+                        artistId = "a1",
+                        name = "Artist name in this release group",
+                        joinPhrase = "",
+                        order = 0
+                    ),
+                    ReleaseGroupArtistCreditRoomModel(
+                        releaseGroupId = "rg2",
+                        artistId = "a1",
+                        name = "Artist name in this release group",
+                        joinPhrase = "",
+                        order = 0
+                    )
+                )
+            )
+
+            val foundReleaseGroups = getReleaseGroupsFromPagingSource(
+                releaseGroupDao.getReleaseGroupsByArtistSorted("a1")
+            )
+
+            assertEquals(
+                "and me",
+                foundReleaseGroups[0].name
+            )
+            assertEquals(
+                "Should find me",
+                foundReleaseGroups[1].name
+            )
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getReleaseGroupsByArtistFiltered() {
+        runBlocking {
+            artistDao.insert(
+                ArtistRoomModel(
+                    id = "a1",
+                    name = "Artist"
+                )
+            )
+            releaseGroupDao.insertAll(
+                listOf(
+                    ReleaseGroupRoomModel(
+                        id = "rg1",
+                        name = "Should find me"
+                    ),
+                    ReleaseGroupRoomModel(
+                        id = "rg2",
+                        name = "and me"
+                    ),
+                    ReleaseGroupRoomModel(
+                        id = "rg3",
+                        name = "but not me"
+                    ),
+                )
+            )
+            releaseGroupArtistDao.insertAll(
+                listOf(
+                    ReleaseGroupArtistCreditRoomModel(
+                        releaseGroupId = "rg1",
+                        artistId = "a1",
+                        name = "Artist name in this release group",
+                        joinPhrase = "",
+                        order = 0
+                    ),
+                    ReleaseGroupArtistCreditRoomModel(
+                        releaseGroupId = "rg2",
+                        artistId = "a1",
+                        name = "Artist name in this release group",
+                        joinPhrase = "",
+                        order = 0
+                    )
+                )
+            )
+
+            val foundReleaseGroups = getReleaseGroupsFromPagingSource(
+                releaseGroupDao.getReleaseGroupsByArtistFiltered("a1", "%f%")
+            )
+
+            assertEquals(
+                "Should find me",
+                foundReleaseGroups[0].name
+            )
+            assertEquals(
+                1,
+                foundReleaseGroups.size
+            )
+        }
+    }
+
+    // TODO: filtered and sorted
+
+    // TODO: filter on fields other than name
+
+    // TODO: sort on other fields
+
+    private suspend fun getReleaseGroupsFromPagingSource(
+        pagingSource: PagingSource<Int, ReleaseGroupRoomModel>
+    ): List<ReleaseGroupRoomModel> {
+        val loadResult: PagingSource.LoadResult<Int, ReleaseGroupRoomModel> = pagingSource.load(
+            PagingSource.LoadParams.Refresh(key = null, loadSize = SEARCH_BROWSE_LIMIT, placeholdersEnabled = false)
+        )
+        return (loadResult as PagingSource.LoadResult.Page<Int, ReleaseGroupRoomModel>).data
     }
 }
