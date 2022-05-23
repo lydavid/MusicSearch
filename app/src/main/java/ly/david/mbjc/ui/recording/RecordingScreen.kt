58642eb9fb4980cde6abf669eee3bfe531e6cbfa
David Ly <d.ly@alum.utoronto.ca>
2022-05-22 23:12:09 -0400

    Make sure to use `material3` or else dark mode won't work for our setup

--- app/src/main/java/ly/david/mbjc/ui/recording/RecordingScreen.kt
+++ app/src/main/java/ly/david/mbjc/ui/recording/RecordingScreen.kt
@@ -1,26 +1,15 @@
 package ly.david.mbjc.ui.recording
 
-import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import ly.david.mbjc.data.getNameWithDisambiguation
 import ly.david.mbjc.data.persistence.recording.RecordingRelationRoomModel
-import ly.david.mbjc.ui.common.ClickableListItem
 import ly.david.mbjc.ui.common.paging.PagingLoadingAndErrorHandler
 import ly.david.mbjc.ui.common.rememberFlowWithLifecycleStarted
-import ly.david.mbjc.ui.theme.PreviewTheme
-import ly.david.mbjc.ui.theme.TextStyles
-import ly.david.mbjc.ui.theme.getSubTextColor
 
 // TODO: unlike the screens before, the main screen just displays all the details/rels of this recording
 @Composable
@@ -61,58 +50,5 @@ internal fun RecordingScreen(
             disambiguation = recordingRelation.disambiguation,
             attributes = recordingRelation.attributes
         )
-    }
-}
-
-@Composable
-internal fun RecordingRelationCard(
-    label: String,
-    name: String,
-    disambiguation: String? = null,
-    attributes: String? = null,
-    onClick: () -> Unit = {}
-) {
-    ClickableListItem(onClick = onClick) {
-        Column(modifier = Modifier.padding(vertical = 8.dp)) {
-            Text(
-                text = "$label:",
-                style = TextStyles.getCardBodyTextStyle()
-            )
-            Text(
-                text = name,
-                style = TextStyles.getCardTitleTextStyle()
-            )
-
-            if (!disambiguation.isNullOrEmpty()) {
-                Text(
-                    text = "($disambiguation)",
-                    style = TextStyles.getCardBodyTextStyle(),
-                    color = getSubTextColor()
-                )
-            }
-
-            if (!attributes.isNullOrEmpty()) {
-                Text(
-                    text = "($attributes)",
-                    style = TextStyles.getCardBodyTextStyle(),
-                )
-            }
-        }
-    }
-}
-
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-internal fun RecordingRelationCardPreview() {
-    PreviewTheme {
-        Surface {
-            RecordingRelationCard(
-                label = "miscellaneous support",
-                name = "Artist Name",
-                disambiguation = "that guy",
-                attributes = "task: director & organizer, label: value"
-            )
-        }
     }
 }
