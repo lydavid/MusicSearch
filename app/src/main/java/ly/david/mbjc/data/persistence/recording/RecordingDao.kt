07b7c4eeb11920e0199063e535fe4a356e4fd817
David Ly <d.ly@alum.utoronto.ca>
2022-05-23 20:07:48 -0400

    Clean up

--- app/src/main/java/ly/david/mbjc/data/persistence/recording/RecordingDao.kt
+++ app/src/main/java/ly/david/mbjc/data/persistence/recording/RecordingDao.kt
@@ -8,27 +8,6 @@ import ly.david.mbjc.data.persistence.RecordingRoomModel
 @Dao
 internal abstract class RecordingDao : BaseDao<RecordingRoomModel> {
 
-    // TODO: get recording with all relationships
-
     @Query("SELECT * FROM recordings WHERE id = :recordingId")
     abstract suspend fun getRecording(recordingId: String): RecordingRoomModel?
-
-//    SELECT r.*
-//    FROM releases r
-//    INNER JOIN releases_release_groups rrg ON r.id = rrg.release_id
-//    INNER JOIN release_groups rg ON rg.id = rrg.release_group_id
-//    WHERE rg.id = :releaseGroupId
-    // TODO: incorrect???
-    //  we need to get Relation as final result
-    //  get all recording ids
-//    @Query(
-//        """
-//            SELECT rel.*
-//            FROM relations rel
-//            INNER JOIN recordings_relations rr ON rel.resource_id = rr.linked_resource_id
-//            INNER JOIN recordings rec ON rr.recording_id = rec.id
-//            WHERE rec.id = :recordingId
-//        """
-//    )
-//    abstract fun getRelationsForRecording(recordingId: String): PagingSource<Int, RecordingRelationRoomModel>
 }
