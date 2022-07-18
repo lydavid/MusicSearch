d2e3916a89fdabc89b98b4e1dfd251ee883ee32b
David Ly <d.ly@alum.utoronto.ca>
2022-07-18 18:35:23 -0400

    Preview material3 AlertDialog

--- app/src/main/java/ly/david/mbjc/ui/common/SimpleAlertDialog.kt
+++ app/src/main/java/ly/david/mbjc/ui/common/SimpleAlertDialog.kt
@@ -1,9 +1,13 @@
 package ly.david.mbjc.ui.common
 
+import android.content.res.Configuration
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ly.david.mbjc.ui.theme.PreviewTheme
 
 @Composable
 internal fun SimpleAlertDialog(
@@ -24,4 +28,24 @@ internal fun SimpleAlertDialog(
     )
 }
 
-// TODO: preview. currently can't due to api
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun Preview() {
+    PreviewTheme {
+        Surface {
+            SimpleAlertDialog(title = "Title", confirmText = "OK") {}
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun Long() {
+    PreviewTheme {
+        Surface {
+            SimpleAlertDialog(title = "Title text that is long enough to wrap", confirmText = "OK") {}
+        }
+    }
+}
