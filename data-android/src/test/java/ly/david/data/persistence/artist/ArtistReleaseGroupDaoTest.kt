7bb72305b67cf26e2708a6719062efd4fc36cdc7
David Ly <d.ly@alum.utoronto.ca>
2022-11-20 17:57:47 -0500

    Test delete

--- data-android/src/test/java/ly/david/data/persistence/artist/ArtistReleaseGroupDaoTest.kt
+++ data-android/src/test/java/ly/david/data/persistence/artist/ArtistReleaseGroupDaoTest.kt
@@ -74,7 +74,8 @@ internal class ArtistReleaseGroupDaoTest : HiltTest() {
             artistReleaseGroupDao.getReleaseGroupsByArtist("a1")
         )
 
-        assertThat("Should find me", `is`(releaseGroupWithArtistCredits[0].releaseGroup.name)
+        assertThat(
+            "Should find me", `is`(releaseGroupWithArtistCredits[0].releaseGroup.name)
         )
         assertThat("and me", `is`(releaseGroupWithArtistCredits[1].releaseGroup.name))
     }
@@ -279,12 +280,79 @@ internal class ArtistReleaseGroupDaoTest : HiltTest() {
         )
 
         val releaseGroupWithArtistCredits = getReleaseGroupsFromPagingSource(
-            artistReleaseGroupDao.getReleaseGroupsByArtistFilteredSorted("a1", "%Album%")
+            artistReleaseGroupDao.getReleaseGroupsByArtistFiltered("a1", "%Album%")
         )
 
         assertEquals(2, releaseGroupWithArtistCredits.size)
         assertEquals("blah", releaseGroupWithArtistCredits[0].releaseGroup.name)
         assertEquals("and me", releaseGroupWithArtistCredits[1].releaseGroup.name)
+    }
+
+    @Test
+    fun `delete release groups by artist`() = runTest {
+        artistDao.insertAll(
+            listOf(
+                ArtistRoomModel(
+                    id = "a1",
+                    name = "Artist"
+                ),
+                ArtistRoomModel(
+                    id = "a2",
+                    name = "Other Artist"
+                ),
+            )
+        )
+        releaseGroupDao.insertAll(
+            listOf(
+                ReleaseGroupRoomModel(
+                    id = "rg1",
+                    name = "I will be deleted",
+                    primaryType = "Single"
+                ),
+                ReleaseGroupRoomModel(
+                    id = "rg2",
+                    name = "so will I, even though I belong to another artist as well",
+                    primaryType = "Album"
+                ),
+                ReleaseGroupRoomModel(
+                    id = "rg3",
+                    name = "I'm still here",
+                    primaryType = "Album"
+                ),
+            )
+        )
+        artistReleaseGroupDao.insertAll(
+            listOf(
+                ArtistReleaseGroup(
+                    releaseGroupId = "rg1",
+                    artistId = "a1",
+                ),
+                ArtistReleaseGroup(
+                    releaseGroupId = "rg2",
+                    artistId = "a1",
+                ),
+                ArtistReleaseGroup(
+                    releaseGroupId = "rg2",
+                    artistId = "a2",
+                ),
+                ArtistReleaseGroup(
+                    releaseGroupId = "rg3",
+                    artistId = "a2"
+                )
+            )
+        )
+
+        artistReleaseGroupDao.deleteReleaseGroupsByArtist("a1")
+        var releaseGroupWithArtistCredits = getReleaseGroupsFromPagingSource(
+            artistReleaseGroupDao.getReleaseGroupsByArtist("a1")
+        )
+        assertEquals(0, releaseGroupWithArtistCredits.size)
+
+        releaseGroupWithArtistCredits = getReleaseGroupsFromPagingSource(
+            artistReleaseGroupDao.getReleaseGroupsByArtist("a2")
+        )
+        assertEquals(1, releaseGroupWithArtistCredits.size)
+        assertEquals("I'm still here", releaseGroupWithArtistCredits[0].releaseGroup.name)
     }
 
     // TODO: generic
