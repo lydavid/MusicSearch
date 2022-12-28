9b8324e04d7b7133db76f5d862e3b660bb599ea0
David Ly <d.ly@alum.utoronto.ca>
2022-12-27 22:02:18 -0500

    Hack-support for filtering settings from viewmodel
    - This is not based on localized string so only works on hardcoded strings

--- app/src/main/java/ly/david/mbjc/ui/settings/SettingsScaffold.kt
+++ app/src/main/java/ly/david/mbjc/ui/settings/SettingsScaffold.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import java.util.regex.Pattern
 import ly.david.mbjc.BuildConfig
 import ly.david.mbjc.ExcludeFromJacocoGeneratedReport
 import ly.david.mbjc.R
@@ -26,6 +25,7 @@ import ly.david.mbjc.ui.theme.PreviewTheme
 @Composable
 fun SettingsScaffold(
     openDrawer: () -> Unit = {},
+    viewModel: SettingsViewModel = hiltViewModel()
 ) {
 
     var filterText by rememberSaveable { mutableStateOf("") }
@@ -38,37 +38,47 @@ fun SettingsScaffold(
                 filterText = filterText,
                 onFilterTextChange = {
                     filterText = it
+                    viewModel.setFilter(filterText)
                 },
             )
         },
     ) {
-        SettingsScreen(filterText)
+
+        val showTheme by viewModel.showTheme.collectAsState(initial = true)
+        val theme by viewModel.appPreferences.themeFlow.collectAsState(initial = AppPreferences.Theme.SYSTEM)
+        val showAppVersion by viewModel.showAppVersion.collectAsState(initial = true)
+
+        SettingsScreen(
+            showTheme = showTheme,
+            showAppVersion = showAppVersion,
+            theme = theme,
+            onThemeChange = { viewModel.appPreferences.setTheme(it) },
+        )
     }
 }
 
 @Composable
 fun SettingsScreen(
-    filterText: String,
-    viewModel: SettingsViewModel = hiltViewModel()
+    showTheme: Boolean = true,
+    theme: AppPreferences.Theme,
+    showAppVersion: Boolean = true,
+    onThemeChange: (AppPreferences.Theme) -> Unit = {}
 ) {
-
-    // TODO: can we filter through viewmodel? pass filter data to viewmodel,
-    //  then it decides what to show
-    val filterRegex = Regex(".*(?i)(${Pattern.quote(filterText)}).*")
 
     Column {
 
-        val theme by viewModel.appPreferences.themeFlow.collectAsState(initial = AppPreferences.Theme.SYSTEM)
-        SettingWithDialogChoices(
-            titleRes = R.string.theme,
-            choices = AppPreferences.Theme.values().map { stringResource(id = it.textRes) },
-            selectedChoiceIndex = theme.ordinal,
-            onSelectChoiceIndex = { viewModel.appPreferences.setTheme(AppPreferences.Theme.values()[it]) },
-        )
+        if (showTheme) {
+            SettingWithDialogChoices(
+                titleRes = R.string.theme,
+                choices = AppPreferences.Theme.values().map { stringResource(id = it.textRes) },
+                selectedChoiceIndex = theme.ordinal,
+                onSelectChoiceIndex = { onThemeChange(AppPreferences.Theme.values()[it]) },
+            )
+        }
 
         val versionKey = stringResource(id = R.string.app_version)
-        val versionValue = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
-        if (filterRegex.matches(versionKey) || filterRegex.matches(versionValue)) {
+        val versionValue = BuildConfig.VERSION_NAME
+        if (showAppVersion) {
             TextWithHeading(heading = versionKey, text = versionValue)
         }
 
@@ -77,22 +87,23 @@ fun SettingsScreen(
         //  other entities have a sort_name field in backend
         //  but doesn't seem to be exposed for editing/displaying
 
-        if (BuildConfig.DEBUG) {
-            DevSettingsSection(
-                viewModel = viewModel
-            )
-        }
+//        if (BuildConfig.DEBUG) {
+//            DevSettingsSection(
+//                viewModel = viewModel
+//            )
+//        }
     }
 }
 
-// TODO: broken because of regex?
 @ExcludeFromJacocoGeneratedReport
 @DefaultPreviews
 @Composable
 private fun Preview() {
     PreviewTheme {
         Surface {
-            SettingsScaffold()
+            SettingsScreen(
+                theme = AppPreferences.Theme.SYSTEM
+            )
         }
     }
 }
--- app/src/main/java/ly/david/mbjc/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/ly/david/mbjc/ui/settings/SettingsViewModel.kt
@@ -9,21 +9,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.io.IOException
+import java.util.regex.Pattern
 import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import ly.david.mbjc.BuildConfig
 
 private val SHOW_THING = booleanPreferencesKey("show_thing")
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val preferencesDataStore: DataStore<Preferences>,
     val appPreferences: AppPreferences
 ) : ViewModel() {
 
+    // TODO: this type of setting should go in AppPreferences
     // Issue for StateFlow so we don't have to specify defaults twice: https://issuetracker.google.com/issues/196441778
     val showThingFlow: Flow<Boolean>
         get() = preferencesDataStore.data
@@ -44,5 +52,35 @@ class SettingsViewModel @Inject constructor(
                 it[SHOW_THING] = show
             }
         }
+    }
+
+    private val filter: MutableStateFlow<String> = MutableStateFlow("")
+
+    // TODO: can we avoid flow { emit { ?
+    private val regex: Flow<Regex> = filter.flatMapLatest { filter ->
+        flow {
+            emit(Regex(".*(?i)(${Pattern.quote(filter)}).*"))
+        }
+    }
+
+    val showTheme: Flow<Boolean> =
+        regex.flatMapLatest { regex ->
+            flow {
+                // TODO: should use passed in stringRes
+                //  but consider removing this feature and waiting for search support
+                //  which was built-in to xml (see Anki Android)
+                emit(regex.matches("Theme") || regex.matches("Light") || regex.matches("Dark") || regex.matches("System"))
+            }
+        }
+
+    val showAppVersion: Flow<Boolean> =
+        regex.flatMapLatest { regex ->
+            flow {
+                emit(regex.matches("App version") || regex.matches(BuildConfig.VERSION_NAME))
+            }
+        }
+
+    fun setFilter(filterText: String) {
+        this.filter.value = filterText
     }
 }
