cb9f758543502637898035e38498e71c5ea90728
David Ly <d.ly@alum.utoronto.ca>
2023-01-27 22:43:57 -0500

    Revert "Fail a test in a module"
    This reverts commit 4b980cccae122a58994e2179aec434f4686bd277.

--- data/coverart/src/test/java/ly/david/data/coverart/TrimCoverArtSuffixTests.kt
+++ data/coverart/src/test/java/ly/david/data/coverart/TrimCoverArtSuffixTests.kt
@@ -17,7 +17,7 @@ class TrimCoverArtSuffixTests {
     fun `non-thumbnail`() {
         assertEquals(
             "https://coverartarchive.org/release/e6a9a248-649c-4be1-bc84-924638bafa49/32187188956",
-            "https://coverartarchive.org/release/e6a9a248-649c-4be1-bc84-924638bafa49/32187188956-50".trimCoverArtSuffix()
+            "https://coverartarchive.org/release/e6a9a248-649c-4be1-bc84-924638bafa49/32187188956-500".trimCoverArtSuffix()
         )
     }
 }
