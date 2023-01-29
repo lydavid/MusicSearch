5daa89c3931a7dccfa5a8e9510abfc1fbf50fe2c
David Ly <d.ly@alum.utoronto.ca>
2023-01-29 10:35:50 -0500

    Fix RELATION_QUERY_WITHOUT_TRANSACTION warnings
    - See: https://developer.android.com/reference/androidx/room/RoomWarnings#RELATION_QUERY_WITHOUT_TRANSACTION()
    
    ```
    warning: The return value includes a POJO with a @relation. It is usually desired to annotate this method with @transaction to avoid possibility of inconsistent results between the POJO and its relations. See https://developer.android.com/reference/androidx/room/Transaction.html for details. - ly.david.data.persistence.work.WorkDao.getWork(java.lang.String)
    ```

--- data/src/main/java/ly/david/data/persistence/area/AreaDao.kt
+++ data/src/main/java/ly/david/data/persistence/area/AreaDao.kt
@@ -4,11 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import ly.david.data.persistence.BaseDao
 
 @Dao
 abstract class AreaDao : BaseDao<AreaRoomModel>() {
 
+    @Transaction
     @Query("SELECT * FROM area WHERE id = :areaId")
     abstract suspend fun getArea(areaId: String): AreaWithIso?
 
--- data/src/main/java/ly/david/data/persistence/work/WorkDao.kt
+++ data/src/main/java/ly/david/data/persistence/work/WorkDao.kt
@@ -4,11 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import ly.david.data.persistence.BaseDao
 
 @Dao
 abstract class WorkDao : BaseDao<WorkRoomModel>() {
 
+    @Transaction
     @Query("SELECT * FROM work WHERE id = :workId")
     abstract suspend fun getWork(workId: String): WorkWithAttributes?
 
