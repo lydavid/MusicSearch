5240d64878e7a03efdb8264a67f5460d5dbe3954
David Ly <d.ly@alum.utoronto.ca>
2023-02-12 15:14:29 -0500

    Convert top-level build.gradle to kts; declare sdk versions in top-level once; start using gradle version catalog
    https://docs.gradle.org/current/userguide/platforms.html

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.BasePlugin
+import io.gitlab.arturbosch.detekt.Detekt
+
+plugins {
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.google.dagger.hilt.android") version "2.45" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.22.0" apply true
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    plugins.withType<BasePlugin>().configureEach {
+        extensions.configure<BaseExtension> {
+            compileSdkVersion(33)
+            defaultConfig {
+                minSdk = 23
+                targetSdk = 33
+            }
+
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+        dependencies {
+            detektPlugins("com.twitter.compose.rules:detekt:0.0.26")
+        }
+    }
+
+    // ./gradlew assembleRelease -Pmbjc.enableComposeCompilerReports=true --rerun-tasks
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.addAll("-opt-in=kotlin.RequiresOptIn")
+
+            if (project.findProperty("mbjc.enableComposeCompilerReports") == "true") {
+                freeCompilerArgs.addAll(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                        project.buildDir.absolutePath + "/compose_metrics"
+                )
+                freeCompilerArgs.addAll(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                        project.buildDir.absolutePath + "/compose_metrics"
+                )
+            }
+        }
+    }
+
+    detekt {
+        buildUponDefaultConfig = true
+        allRules = false
+        parallel = true
+
+        // Use a single config file
+        config = files("${project.rootDir}/config/detekt.yml")
+
+        // Each module has its own baseline otherwise they overwrite each other
+        baseline = file("${projectDir}/config/baseline.xml")
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            // observe findings in your browser with structure and code snippets
+            html.required.set(true)
+
+            // similar to the console output, contains issue signature to manually edit baseline files
+            txt.required.set(true)
+        }
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,64 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+// https://docs.gradle.org/current/userguide/platforms.html
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+
+            // https://developer.android.com/jetpack/androidx/releases/compose
+            version("compose-foundation", "1.3.1")
+            version("compose-material", "1.3.1")
+            version("compose-material3", "1.0.1")
+            version("compose-ui", "1.3.3")
+
+            version("hilt", "2.45")
+            version("hilt-navigation", "1.0.0")
+            version("moshi", "1.14.0")
+            version("retrofit", "2.9.0")
+            version("room", "2.5.0")
+
+            library("compose-foundation", "androidx.compose.foundation", "foundation").versionRef("compose-foundation")
+            library("compose-material", "androidx.compose.material", "material").versionRef("compose-material")
+            library("compose-material-icons-extended", "androidx.compose.material", "material-icons-extended").versionRef("compose-material")
+            library("compose-ui", "androidx.compose.ui", "ui").versionRef("compose-ui")
+            library("compose-ui-preview", "androidx.compose.ui", "ui-tooling-preview").versionRef("compose-ui")
+            library("compose-ui-test", "androidx.compose.ui", "ui-test-junit4").versionRef("compose-ui")
+            library("compose-ui-tooling", "androidx.compose.ui", "ui-tooling").versionRef("compose-ui")
+
+            library("dagger", "com.google.dagger", "dagger").versionRef("hilt")
+            library("hilt-android", "com.google.dagger", "hilt-android").versionRef("hilt")
+            library("hilt-android-compiler", "com.google.dagger", "hilt-android-compiler").versionRef("hilt")
+            library("hilt-android-testing", "com.google.dagger", "hilt-android-testing").versionRef("hilt")
+            library("hilt-navigation-compose", "androidx.hilt", "hilt-navigation-compose").versionRef("hilt-navigation")
+
+            library("moshi-kotlin", "com.squareup.moshi", "moshi-kotlin").versionRef("moshi")
+
+            library("retrofit", "com.squareup.retrofit2", "retrofit").versionRef("retrofit")
+            library("retrofit-converter-moshi", "com.squareup.retrofit2", "converter-moshi").versionRef("retrofit")
+
+            library("room-common", "androidx.room", "room-common").versionRef("room")
+            library("room-compiler", "androidx.room", "room-compiler").versionRef("room")
+            library("room-ktx", "androidx.room", "room-ktx").versionRef("room")
+            library("room-paging", "androidx.room", "room-paging").versionRef("room")
+            library("room-runtime", "androidx.room", "room-runtime").versionRef("room")
+            library("room-testing", "androidx.room", "room-testing").versionRef("room")
+        }
+    }
+}
+
+rootProject.name = "MusicSearch"
+
+include(
+    ":app",
+    ":data-android",
+    ":data",
+    ":test-data",
+    ":data:coverart",
+    ":data:base"
+)
