defc806f8fbea58008cd4d06f98284c60e0f04cc
David Ly <d.ly@alum.utoronto.ca>
2023-03-30 22:02:06 -0400

    Support SwipeToDismiss on top level snackbar
    https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#SwipeToDismiss(androidx.compose.material3.DismissState,kotlin.Function1,kotlin.Function1,androidx.compose.ui.Modifier,kotlin.collections.Set)

--- app/src/main/java/ly/david/mbjc/ui/TopLevelScaffold.kt
+++ app/src/main/java/ly/david/mbjc/ui/TopLevelScaffold.kt
@@ -4,10 +4,13 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.material3.rememberSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -115,7 +118,6 @@ internal fun TopLevelScaffold(
 
                         val snackbarResult = snackbarHostState.showSnackbar(
                             message = addToCollectionResult.message,
-                            withDismissAction = addToCollectionResult.actionLabel == null,
                             actionLabel = addToCollectionResult.actionLabel,
                             duration = SnackbarDuration.Short
                         )
@@ -140,7 +142,15 @@ internal fun TopLevelScaffold(
                 navigateToTopLevelDestination = { it.onTopLevelDestinationClick() }
             )
         },
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { snackbarData ->
+                SwipeToDismiss(
+                    state = rememberDismissState(),
+                    background = {},
+                    dismissContent = { Snackbar(snackbarData) }
+                )
+            }
+        },
     ) { innerPadding ->
 
         NavigationGraph(
