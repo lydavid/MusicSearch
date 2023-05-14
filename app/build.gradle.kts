81180e46c5624fa7bae0038bbaeda0c3d535215b
David Ly <d.ly@alum.utoronto.ca>
2023-05-14 12:43:17 -0400

    chore: use version catalog for all app dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,78 +96,59 @@ dependencies {
     testImplementation(project(":test-data"))
     androidTestImplementation(project(":test-data"))
 
-    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+    implementation(libs.accompanist.swiperefresh)
+    implementation(libs.accompanist.pager)
 
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.constraintlayout.compose)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.paging.runtime)
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation(libs.appauth)
+
+    implementation(libs.coil.base)
+    implementation(libs.coil.compose)
+    androidTestImplementation(libs.coil.test)
 
     implementation(libs.compose.foundation)
+    implementation(libs.compose.material.icons.extended)
+    implementation(libs.compose.material3)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.preview)
     androidTestImplementation(libs.compose.ui.test)
     debugImplementation(libs.compose.ui.tooling)
 
-    implementation(libs.compose.material.icons.extended)
-    implementation(libs.compose.material3)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
 
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-
-    implementation(libs.accompanist.swiperefresh)
-    implementation(libs.accompanist.pager)
-
-    // Paging
-    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
-
-    // https://developer.android.com/jetpack/androidx/releases/paging#1.0.0-alpha15
-    // LazyPagingItems now sets the initial loadState to have a LoadState.Loading refresh.
-    // https://issuetracker.google.com/issues/224855902
-    implementation("androidx.paging:paging-compose:1.0.0-alpha14")
-
-    // Retrofit
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.moshi)
-
-    // Room
-    androidTestImplementation(libs.room.testing)
-
-    // Hilt
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     kapt(libs.hilt.android.compiler)
     kaptAndroidTest(libs.hilt.android.compiler)
     androidTestImplementation(libs.hilt.android.testing)
 
-    implementation(libs.coil.base)
-    implementation(libs.coil.compose)
-    androidTestImplementation(libs.coil.test)
-
-    // Tried this for UScript.getName(UScript.getCodeFromName(it))
-//    implementation "com.ibm.icu:icu4j:72.1"
-
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    implementation("com.jakewharton.timber:timber:5.0.1")
-
-    implementation("net.openid:appauth:0.11.1")
-
-    // Test - General
     testImplementation(libs.junit)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
-    androidTestImplementation("com.squareup.okhttp3:okhttp-tls:4.11.0")
+    testImplementation(libs.bundles.kotlinx.coroutines)
+    androidTestImplementation(libs.bundles.kotlinx.coroutines)
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    implementation(libs.bundles.retrofit)
+
+    androidTestImplementation(libs.room.testing)
+
+    androidTestImplementation(libs.okhttp.mockwebserver)
+    androidTestImplementation(libs.okhttp.tls)
+
+    implementation(libs.timber)
+
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.arch.core.testing)
 }
 
 if (file("google-services.json").exists()) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,13 +21,29 @@ dependencyResolutionManagement {
             version("hilt", "2.46.1")
             version("hilt-navigation", "1.0.0")
             version("junit", "4.13.2")
+            version("kotlinx-coroutines", "1.6.4")
             version("mockk", "1.13.5")
             version("moshi", "1.15.0")
+            version("okhttp", "4.11.0")
             version("retrofit", "2.9.0")
             version("room", "2.5.1")
 
             library("accompanist-swiperefresh", "com.google.accompanist", "accompanist-swiperefresh").versionRef("accompanist")
             library("accompanist-pager", "com.google.accompanist", "accompanist-pager").versionRef("accompanist")
+
+            library("androidx-activity-compose", "androidx.activity", "activity-compose").version("1.7.1")
+            library("androidx-arch-core-testing", "androidx.arch.core", "core-testing").version("2.2.0")
+            library("androidx-constraintlayout-compose", "androidx.constraintlayout", "constraintlayout-compose").version("1.0.1")
+            library("androidx-core", "androidx.core", "core-ktx").version("1.10.1")
+            library("androidx-datastore-preferences", "androidx.datastore", "datastore-preferences").version("1.0.0")
+            library("androidx-lifecycle-viewmodel-compose", "androidx.lifecycle", "lifecycle-viewmodel-compose").version("2.6.1")
+            library("androidx-navigation-compose", "androidx.navigation", "navigation-compose").version("2.5.3")
+            library("androidx-paging-compose", "androidx.paging", "paging-compose").version("1.0.0-alpha14")
+            library("androidx-paging-runtime", "androidx.paging", "paging-runtime-ktx").version("3.1.1")
+            library("androidx-test-junit", "androidx.test.ext", "junit").version("1.1.5")
+            library("androidx-test-espresso-core", "androidx.test.espresso", "espresso-core").version("3.5.1")
+
+            library("appauth", "net.openid", "appauth").version("0.11.1")
 
             library("coil-base", "io.coil-kt", "coil-base").versionRef("coil")
             library("coil-compose", "io.coil-kt", "coil-compose").versionRef("coil")
@@ -44,6 +60,11 @@ dependencyResolutionManagement {
             library("compose-ui-tooling", "androidx.compose.ui", "ui-tooling").versionRef("compose-ui")
 
             library("dagger", "com.google.dagger", "dagger").versionRef("hilt")
+
+            library("firebase-bom", "com.google.firebase", "firebase-bom").version("32.0.0")
+            library("firebase-analytics", "com.google.firebase", "firebase-analytics-ktx").version("21.2.2")
+            library("firebase-crashlytics", "com.google.firebase", "firebase-crashlytics-ktx").version("18.3.7")
+
             library("hilt-android", "com.google.dagger", "hilt-android").versionRef("hilt")
             library("hilt-android-compiler", "com.google.dagger", "hilt-android-compiler").versionRef("hilt")
             library("hilt-android-testing", "com.google.dagger", "hilt-android-testing").versionRef("hilt")
@@ -51,12 +72,20 @@ dependencyResolutionManagement {
 
             library("junit", "junit", "junit").versionRef("junit")
 
+            library("kotlinx-coroutines-android", "org.jetbrains.kotlinx", "kotlinx-coroutines-android").versionRef("kotlinx-coroutines")
+            library("kotlinx-coroutines-test", "org.jetbrains.kotlinx", "kotlinx-coroutines-test").versionRef("kotlinx-coroutines")
+            bundle("kotlinx-coroutines", listOf("kotlinx-coroutines-android", "kotlinx-coroutines-test"))
+
             library("mockk", "io.mockk", "mockk").versionRef("mockk")
 
             library("moshi-kotlin", "com.squareup.moshi", "moshi-kotlin").versionRef("moshi")
 
+            library("okhttp-mockwebserver", "com.squareup.okhttp3", "mockwebserver").versionRef("okhttp")
+            library("okhttp-tls", "com.squareup.okhttp3", "okhttp-tls").versionRef("okhttp")
+
             library("retrofit", "com.squareup.retrofit2", "retrofit").versionRef("retrofit")
             library("retrofit-converter-moshi", "com.squareup.retrofit2", "converter-moshi").versionRef("retrofit")
+            bundle("retrofit", listOf("retrofit", "retrofit-converter-moshi"))
 
             library("room-common", "androidx.room", "room-common").versionRef("room")
             library("room-compiler", "androidx.room", "room-compiler").versionRef("room")
@@ -64,6 +93,8 @@ dependencyResolutionManagement {
             library("room-paging", "androidx.room", "room-paging").versionRef("room")
             library("room-runtime", "androidx.room", "room-runtime").versionRef("room")
             library("room-testing", "androidx.room", "room-testing").versionRef("room")
+
+            library("timber", "com.jakewharton.timber", "timber").version("5.0.1")
         }
     }
 }
