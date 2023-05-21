fa56ec826b642cf7e6786afc00fe983ac6beff5c
David Ly <d.ly@alum.utoronto.ca>
2023-05-21 16:00:22 -0400

    test both light and dark mode

---
+++ ui/common/src/test/java/ly/david/ui/common/PaparazziScreenshotTest.kt
@@ -0,0 +1,44 @@
+package ly.david.ui.common
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.resources.NightMode
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * Common setup for running screenshot tests on light and dark mode.
+ *
+ * Unfortunately, implementing class must also be annotated with:
+ * ```
+ * @RunWith(Parameterized::class)
+ * ```
+ *
+ * and declare the parameters:
+ * ```
+ * config: DeviceConfig
+ * ```
+ */
+@RunWith(Parameterized::class)
+abstract class PaparazziScreenshotTest( // Must be abstract. Ignore Detekt.
+    config: DeviceConfig
+) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<DeviceConfig> {
+            return listOf(
+                DeviceConfig.PIXEL_5,
+                DeviceConfig.PIXEL_5.copy(nightMode = NightMode.NIGHT)
+            )
+        }
+    }
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = config,
+        theme = "android:Theme.Material.Light.NoActionBar"
+    )
+}
--- ui/common/src/test/java/ly/david/ui/common/dialog/SimpleAlertDialogTest.kt
+++ ui/common/src/test/java/ly/david/ui/common/dialog/SimpleAlertDialogTest.kt
@@ -1,19 +1,17 @@
 package ly.david.ui.common.dialog
 
 import androidx.compose.material3.Surface
-import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_5
-import app.cash.paparazzi.Paparazzi
-import com.android.resources.NightMode
+import app.cash.paparazzi.DeviceConfig
+import ly.david.ui.common.PaparazziScreenshotTest
 import ly.david.ui.common.theme.PreviewTheme
-import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-class SimpleAlertDialogTest {
-    @get:Rule
-    val paparazzi = Paparazzi(
-        deviceConfig = PIXEL_5.copy(nightMode = NightMode.NIGHT),
-        theme = "android:Theme.Material.Light.NoActionBar",
-    )
+@RunWith(Parameterized::class)
+class SimpleAlertDialogTest(
+    config: DeviceConfig
+): PaparazziScreenshotTest(config) {
 
     @Test
     fun long() {
