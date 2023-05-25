71d25cbedae65f7c055eaae068bf5cf5e6866cf0
lydavid <lydavid@users.noreply.github.com>
2023-05-25 07:07:28 +0000

    chore: bump version [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "io.github.lydavid.musicsearch"
-        versionCode = 77
-        versionName = "0.7.5"
+        versionCode = 78
+        versionName = "0.7.6"
 
         testInstrumentationRunner = "ly.david.mbjc.CustomTestRunner"
         vectorDrawables {
