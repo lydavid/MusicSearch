e07895a57f9c36273416158c27f716de72bc5056
David Ly <d.ly@alum.utoronto.ca>
2023-05-26 18:59:33 -0400

    fix aab path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.lydavid.musicsearch"
-        versionCode = 79
+        versionCode = 80
         versionName = "0.7.7"
 
         testInstrumentationRunner = "ly.david.mbjc.CustomTestRunner"
