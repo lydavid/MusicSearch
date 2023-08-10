cdfae50b206daf8ece6efaaeca87331f61097f41
David Ly <d.ly@alum.utoronto.ca>
2023-08-09 22:18:03 -0400

    fix: make sure clickable list item end icon doesn't get pushed off screen by long text

--- ui/common/src/main/java/ly/david/ui/common/component/ClickableItem.kt
+++ ui/common/src/main/java/ly/david/ui/common/component/ClickableItem.kt
@@ -1,9 +1,8 @@
 package ly.david.ui.common.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -12,10 +11,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import ly.david.ui.core.preview.DefaultPreviews
 import ly.david.ui.core.theme.PreviewTheme
 import ly.david.ui.core.theme.TextStyles
@@ -28,14 +28,23 @@ fun ClickableItem(
     endIcon: ImageVector? = null,
     onClick: () -> Unit = {},
 ) {
-    Row(
+    ConstraintLayout(
         modifier = modifier
             .fillMaxWidth()
             .clickable { onClick() }
             .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Column {
+        val (text, icon) = createRefs()
+
+        Column(
+            modifier = Modifier.constrainAs(text) {
+                width = Dimension.fillToConstraints
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(icon.start)
+            }
+        ) {
             Text(
                 text = title,
                 style = TextStyles.getCardBodyTextStyle()
@@ -49,13 +58,19 @@ fun ClickableItem(
             }
         }
 
-        Spacer(modifier = Modifier.weight(1f))
-
-        endIcon?.let {
-            Icon(
-                imageVector = endIcon,
-                contentDescription = null,
-            )
+        Box(
+            modifier = Modifier.constrainAs(icon) {
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }
+        ) {
+            endIcon?.let {
+                Icon(
+                    imageVector = endIcon,
+                    contentDescription = null,
+                )
+            }
         }
     }
 }
@@ -92,7 +107,7 @@ internal fun PreviewClickableItemWithIcon() {
     PreviewTheme {
         Surface {
             ClickableItem(
-                title = "Click me",
+                title = "Click me, my text is so long it will possibly push the end icon off the screen",
                 endIcon = Icons.Default.ChevronRight,
             )
         }
