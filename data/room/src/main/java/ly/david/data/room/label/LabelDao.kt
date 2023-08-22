3289a1b39bb29ee9060594a1f14e2ffcebfe496a
David Ly <d.ly@alum.utoronto.ca>
2023-08-21 20:01:43 -0400

    chore: annotate with @Transaction
    resolves:
    w: [ksp] The return value includes a POJO with a @Relation. It is usually desired to annotate this method with @Transaction to avoid possibility of inconsistent results between the POJO and its relations. See https://developer.android.com/reference/androidx/room/Transaction.html for details. - ly.david.data.room.instrument.InstrumentDao.getInstrument(java.lang.String)

--- data/room/src/main/java/ly/david/data/room/label/LabelDao.kt
+++ data/room/src/main/java/ly/david/data/room/label/LabelDao.kt
@@ -2,11 +2,13 @@ package ly.david.data.room.label
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import ly.david.data.room.BaseDao
 
 @Dao
 abstract class LabelDao : BaseDao<LabelRoomModel>() {
 
+    @Transaction
     @Query("SELECT * FROM label WHERE id = :labelId")
     abstract suspend fun getLabel(labelId: String): LabelWithAllData?
 }
--- data/room/src/main/java/ly/david/data/room/series/SeriesDao.kt
+++ data/room/src/main/java/ly/david/data/room/series/SeriesDao.kt
@@ -2,11 +2,13 @@ package ly.david.data.room.series
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import ly.david.data.room.BaseDao
 
 @Dao
 abstract class SeriesDao : BaseDao<SeriesRoomModel>() {
 
+    @Transaction
     @Query("SELECT * FROM series WHERE id = :seriesId")
     abstract suspend fun getSeries(seriesId: String): SeriesWithAllData?
 }
