821667763fd7b45bef5541aff831118859ec414a
David Ly <d.ly@alum.utoronto.ca>
2023-09-01 08:15:05 -0400

    fix screenshot tests

--- ui/common/src/test/java/ly/david/ui/common/place/PlaceListItemTest.kt
+++ ui/common/src/test/java/ly/david/ui/common/place/PlaceListItemTest.kt
@@ -1,7 +1,7 @@
 package ly.david.ui.common.place
 
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import ly.david.data.LifeSpanMusicBrainzModel
+import ly.david.data.LifeSpanUiModel
 import ly.david.data.domain.listitem.PlaceListItemModel
 import ly.david.ui.test.screenshot.PaparazziScreenshotTest
 import org.junit.Test
@@ -33,7 +33,7 @@ class PlaceListItemTest : PaparazziScreenshotTest() {
                     disambiguation = "NYC, closed 2007",
                     type = "Studio",
                     address = "460 W. 54th St., at 10th Avenue, Manhatten, NY",
-                    lifeSpan = LifeSpanMusicBrainzModel(begin = "1993", end = "2007-08", ended = true)
+                    lifeSpan = LifeSpanUiModel(begin = "1993", end = "2007-08", ended = true)
                 )
             )
         }
