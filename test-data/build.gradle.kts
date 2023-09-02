9de442ac6e3d147fe4614b9538aadce52a3aafd4
David Ly <d.ly@alum.utoronto.ca>
2023-09-01 23:22:47 -0400

    chore: clean up

--- test-data/build.gradle.kts
+++ test-data/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("ly.david.android.library")
-    id("kotlin-kapt")
-    alias(libs.plugins.ksp)
 }
 
 android {
@@ -10,12 +8,4 @@ android {
 
 dependencies {
     api(projects.data)
-
-    api(libs.coil.base)
-    api(libs.coil.compose)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-    kaptTest(libs.hilt.android.compiler)
-    implementation(libs.hilt.android.testing)
-    implementation(libs.room.runtime)
 }
