e7176db7182cf05f0aa9dcb47f0990e6ed72cb3b
David Ly <d.ly@alum.utoronto.ca>
2023-09-13 20:56:28 -0400

    chore: convert :data:musicbrainz to KMP

--- data/musicbrainz/build.gradle.kts
+++ data/musicbrainz/build.gradle.kts
@@ -1,20 +1,33 @@
 plugins {
-    id("ly.david.kotlin")
+    id("ly.david.musicsearch.kotlin.multiplatform")
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
 }
 
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.data.core)
+                implementation(libs.koin.annotations)
+                implementation(libs.koin.core)
+                implementation(project.dependencies.platform(libs.ktor.bom))
+                implementation(libs.ktor.client.auth)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.kotlinx.datetime)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.junit)
+                implementation(libs.koin.test)
+                implementation(libs.mockk)
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation(projects.data.core)
-    implementation(platform(libs.ktor.bom))
-    implementation(libs.bundles.ktor.android)
-    implementation(libs.koin.core)
-    implementation(libs.koin.annotations)
-    implementation(libs.kotlinx.datetime)
-
-    ksp(libs.koin.ksp.compiler)
-
-    testImplementation(libs.junit)
-    testImplementation(libs.koin.test)
-    testImplementation(libs.mockk)
+    add("kspJvm", libs.koin.ksp.compiler)
 }
