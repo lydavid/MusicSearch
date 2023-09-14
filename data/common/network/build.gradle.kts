9d7338950c2976eebf40a0281e59d5a52f6a5b3f
David Ly <d.ly@alum.utoronto.ca>
2023-09-13 21:10:05 -0400

    chore: convert :data:common:network to KMP

--- data/common/network/build.gradle.kts
+++ data/common/network/build.gradle.kts
@@ -1,9 +1,24 @@
 plugins {
-    id("ly.david.kotlin")
+    id("ly.david.musicsearch.kotlin.multiplatform")
     alias(libs.plugins.kotlin.serialization)
 }
 
-dependencies {
-    implementation(platform(libs.ktor.bom))
-    implementation(libs.bundles.ktor.android)
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project.dependencies.platform(libs.ktor.bom))
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.okhttp)
+                implementation(libs.ktor.serialization.kotlinx.json)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.junit)
+                implementation(libs.mockk)
+            }
+        }
+    }
 }
