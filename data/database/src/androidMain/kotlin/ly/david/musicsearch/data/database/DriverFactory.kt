fb9a9c40113395504eb88f9e79f2412c6fd1310a
David Ly <d.ly@alum.utoronto.ca>
2023-09-21 22:58:08 -0400

    enable FK

--- data/database/src/androidMain/kotlin/ly/david/musicsearch/data/database/DriverFactory.kt
+++ data/database/src/androidMain/kotlin/ly/david/musicsearch/data/database/DriverFactory.kt
@@ -1,11 +1,22 @@
 package ly.david.musicsearch.data.database
 
 import android.content.Context
+import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 
 actual class DriverFactory(private val context: Context) {
     actual fun createDriver(): SqlDriver {
-        return AndroidSqliteDriver(Database.Schema, context, "musicsearch.db")
+        return AndroidSqliteDriver(
+            schema = Database.Schema,
+            context = context,
+            name = "musicsearch.db",
+            callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
+                override fun onOpen(db: SupportSQLiteDatabase) {
+                    super.onConfigure(db)
+                    db.setForeignKeyConstraintsEnabled(true)
+                }
+            }
+        )
     }
 }
--- data/database/src/jvmMain/kotlin/ly/david/musicsearch/data/database/DriverFactory.kt
+++ data/database/src/jvmMain/kotlin/ly/david/musicsearch/data/database/DriverFactory.kt
@@ -2,10 +2,14 @@ package ly.david.musicsearch.data.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.util.Properties
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        val driver = JdbcSqliteDriver(
+            url = JdbcSqliteDriver.IN_MEMORY,
+            properties = Properties().apply { put("foreign_keys", "true") }
+        )
         Database.Schema.create(driver)
         return driver
     }
