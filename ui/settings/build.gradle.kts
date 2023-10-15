08b232a94537e99119d03e4deaa054305408a027
David Ly <d.ly@alum.utoronto.ca>
2023-10-14 20:49:33 -0400

    chore: add core so that ly.david.musicsearch.data.core.network.MusicBrainzEntity will be on the classpath

--- ui/settings/build.gradle.kts
+++ ui/settings/build.gradle.kts
@@ -28,6 +28,7 @@ android {
 
 dependencies {
     implementation(projects.core.preferences)
+    implementation(projects.data.core)
     implementation(projects.data.musicbrainz) // TODO: remove after extracting MusicBrainzAuthStore
     implementation(projects.domain) // TODO: remove after extracting destination
     implementation(projects.invertedDomain)
