8d239eb107546a88a6a86b3b511ec74f70e28a39
David Ly <d.ly@alum.utoronto.ca>
2023-10-15 14:08:44 -0400

    chore: remove CollectionPagedList, moving its logic into ViewModel for now

--- ui/collections/src/main/java/ly/david/ui/collections/BrowseCollectionUseCase.kt
+++
@@ -1,8 +0,0 @@
-package ly.david.ui.collections
-
-import androidx.paging.PagingSource
-import ly.david.ui.common.paging.StoreEntityUseCase
-
-interface BrowseCollectionUseCase<DM : Any> : StoreEntityUseCase {
-    fun getLinkedEntitiesPagingSource(viewState: ICollectionPagedList.ViewModelState): PagingSource<Int, DM>
-}
--- ui/collections/src/main/java/ly/david/ui/collections/CollectionListViewModel.kt
+++ ui/collections/src/main/java/ly/david/ui/collections/CollectionListViewModel.kt
@@ -2,19 +2,32 @@ package ly.david.ui.collections
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingData
 import androidx.paging.PagingSource
+import androidx.paging.cachedIn
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import ly.david.data.musicbrainz.api.CollectionApi.Companion.USER_COLLECTIONS
 import ly.david.data.musicbrainz.api.MusicBrainzApi
 import ly.david.data.musicbrainz.auth.MusicBrainzAuthStore
-import ly.david.musicsearch.core.preferences.AppPreferences
 import ly.david.musicsearch.core.models.listitem.CollectionListItemModel
 import ly.david.musicsearch.core.models.network.MusicBrainzEntity
+import ly.david.musicsearch.core.preferences.AppPreferences
 import ly.david.musicsearch.data.database.dao.BrowseEntityCountDao
 import ly.david.musicsearch.data.database.dao.CollectionDao
 import ly.david.musicsearch.domain.browse.usecase.GetBrowseEntityCountUseCase
 import ly.david.musicsearch.domain.collection.usecase.GetAllCollections
+import ly.david.musicsearch.domain.paging.BrowseEntityRemoteMediator
+import ly.david.musicsearch.domain.paging.MusicBrainzPagingConfig
 import lydavidmusicsearchdatadatabase.Browse_entity_count
 import org.koin.android.annotation.KoinViewModel
 
@@ -23,20 +36,74 @@ private const val ONLY_GIVE_ME_LOCAL_COLLECTIONS = "ONLY_GIVE_ME_LOCAL_COLLECTIO
 @KoinViewModel
 class CollectionListViewModel(
     val appPreferences: AppPreferences,
-    private val pagedList: CollectionPagedList,
     private val musicBrainzApi: MusicBrainzApi,
     private val musicBrainzAuthStore: MusicBrainzAuthStore,
     private val collectionDao: CollectionDao,
     private val getAllCollections: GetAllCollections,
     private val browseEntityCountDao: BrowseEntityCountDao,
     private val getBrowseEntityCountUseCase: GetBrowseEntityCountUseCase,
-) : ViewModel(),
-    ICollectionPagedList by pagedList,
-    BrowseCollectionUseCase<CollectionListItemModel> {
+) : ViewModel() {
 
-    init {
-        pagedList.scope = viewModelScope
-        pagedList.useCase = this
+    data class ViewModelState(
+        val entityId: String = "",
+        val query: String = "",
+        val isRemote: Boolean = true,
+        val showLocal: Boolean,
+        val showRemote: Boolean,
+    )
+
+    val entityId: MutableStateFlow<String> = MutableStateFlow("")
+    val query: MutableStateFlow<String> = MutableStateFlow("")
+    val isRemote: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val showLocal: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val showRemote: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    private val paramState = combine(
+        entityId,
+        query,
+        isRemote,
+        showLocal,
+        showRemote,
+    ) { entityId, query, isRemote, showLocal, showRemote ->
+        ViewModelState(
+            entityId,
+            query,
+            isRemote,
+            showLocal,
+            showRemote,
+        )
+    }.distinctUntilChanged()
+
+    @OptIn(
+        ExperimentalPagingApi::class,
+        ExperimentalCoroutinesApi::class,
+    )
+    val pagedEntities: Flow<PagingData<CollectionListItemModel>> by lazy {
+        paramState.filterNot { it.entityId.isEmpty() }
+            .flatMapLatest { state ->
+                Pager(
+                    config = MusicBrainzPagingConfig.pagingConfig,
+                    remoteMediator = getRemoteMediator(state.entityId).takeIf { state.isRemote },
+                    pagingSourceFactory = { getLinkedEntitiesPagingSource(state) },
+                ).flow
+            }
+            .distinctUntilChanged()
+            .cachedIn(viewModelScope)
+    }
+
+    private fun getRemoteMediator(entityId: String) = BrowseEntityRemoteMediator<CollectionListItemModel>(
+        getRemoteEntityCount = { getRemoteLinkedEntitiesCountByEntity(entityId) },
+        getLocalEntityCount = { getLocalLinkedEntitiesCountByEntity(entityId) },
+        deleteLocalEntity = { deleteLinkedEntitiesByEntity(entityId) },
+        browseEntity = { offset ->
+            browseLinkedEntitiesAndStore(
+                entityId,
+                offset,
+            )
+        },
+    )
+
+    fun updateQuery(query: String) {
+        this.query.value = query
     }
 
     fun getUsernameThenLoadCollections() {
@@ -45,9 +112,9 @@ class CollectionListViewModel(
             musicBrainzAuthStore.username.collectLatest { username ->
                 if (username.isEmpty()) {
                     // This id lets the rest of the functions know to avoid network requests
-                    loadPagedEntities(ONLY_GIVE_ME_LOCAL_COLLECTIONS)
+                    entityId.value = ONLY_GIVE_ME_LOCAL_COLLECTIONS
                 } else {
-                    loadPagedEntities(username)
+                    entityId.value = username
                 }
             }
         }
@@ -55,15 +122,18 @@ class CollectionListViewModel(
 
     fun setShowLocal(show: Boolean) {
         appPreferences.setShowLocalCollections(show)
-        updateShowLocal(show)
+        showLocal.value = show
     }
 
     fun setShowRemote(show: Boolean) {
         appPreferences.setShowRemoteCollections(show)
-        updateShowRemote(show)
+        showRemote.value = show
     }
 
-    override suspend fun browseLinkedEntitiesAndStore(entityId: String, nextOffset: Int): Int {
+    private suspend fun browseLinkedEntitiesAndStore(
+        entityId: String,
+        nextOffset: Int,
+    ): Int {
         if (entityId == ONLY_GIVE_ME_LOCAL_COLLECTIONS) return 0
 
         val response = musicBrainzApi.browseCollectionsByUser(
@@ -95,19 +165,25 @@ class CollectionListViewModel(
         return collectionMusicBrainzModels.size
     }
 
-    override suspend fun getRemoteLinkedEntitiesCountByEntity(entityId: String): Int? {
+    private fun getRemoteLinkedEntitiesCountByEntity(entityId: String): Int? {
         if (entityId == ONLY_GIVE_ME_LOCAL_COLLECTIONS) return 0
 
-        return getBrowseEntityCountUseCase(entityId, MusicBrainzEntity.COLLECTION)?.remoteCount
+        return getBrowseEntityCountUseCase(
+            entityId,
+            MusicBrainzEntity.COLLECTION,
+        )?.remoteCount
     }
 
-    override suspend fun getLocalLinkedEntitiesCountByEntity(entityId: String): Int {
+    private fun getLocalLinkedEntitiesCountByEntity(entityId: String): Int {
         if (entityId == ONLY_GIVE_ME_LOCAL_COLLECTIONS) return 0
 
-        return getBrowseEntityCountUseCase(entityId, MusicBrainzEntity.COLLECTION)?.localCount ?: 0
+        return getBrowseEntityCountUseCase(
+            entityId,
+            MusicBrainzEntity.COLLECTION,
+        )?.localCount ?: 0
     }
 
-    override suspend fun deleteLinkedEntitiesByEntity(entityId: String) {
+    private fun deleteLinkedEntitiesByEntity(entityId: String) {
         if (entityId == ONLY_GIVE_ME_LOCAL_COLLECTIONS) return
 
         browseEntityCountDao.withTransaction {
@@ -116,8 +192,8 @@ class CollectionListViewModel(
         }
     }
 
-    override fun getLinkedEntitiesPagingSource(
-        viewState: ICollectionPagedList.ViewModelState,
+    private fun getLinkedEntitiesPagingSource(
+        viewState: ViewModelState,
     ): PagingSource<Int, CollectionListItemModel> =
         collectionDao.getAllCollections(
             showLocal = viewState.showLocal,
