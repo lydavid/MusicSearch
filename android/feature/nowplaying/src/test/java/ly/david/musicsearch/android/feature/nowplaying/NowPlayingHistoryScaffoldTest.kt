09048e1104bb07c8b4bf3bdc07f3353de820ffde
David Ly <d.ly@alum.utoronto.ca>
2023-10-17 23:23:03 -0400

    chore: fix detekt

--- android/feature/nowplaying/src/test/java/ly/david/musicsearch/android/feature/nowplaying/NowPlayingHistoryScaffoldTest.kt
+++ android/feature/nowplaying/src/test/java/ly/david/musicsearch/android/feature/nowplaying/NowPlayingHistoryScaffoldTest.kt
@@ -1,7 +1,6 @@
 package ly.david.musicsearch.android.feature.nowplaying
 
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import ly.david.musicsearch.android.feature.nowplaying.PreviewNowPlayingHistoryScaffold
 import ly.david.ui.test.screenshot.ScreenshotTest
 import org.junit.Test
 import org.junit.runner.RunWith
