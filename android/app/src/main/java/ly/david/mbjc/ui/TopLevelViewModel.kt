39fa087274a8a67ecba5fd9b6478716215b364eb
David Ly <22302001+lydavid@users.noreply.github.com>
2024-02-26 23:10:00 -0500

    fix

--- android/app/src/main/java/ly/david/mbjc/ui/TopLevelViewModel.kt
+++ android/app/src/main/java/ly/david/mbjc/ui/TopLevelViewModel.kt
@@ -19,11 +19,9 @@ import ly.david.musicsearch.data.database.INSERTION_FAILED_DUE_TO_CONFLICT
 import ly.david.musicsearch.data.database.dao.CollectionDao
 import ly.david.musicsearch.data.database.dao.CollectionEntityDao
 import ly.david.musicsearch.data.musicbrainz.auth.LoginAndroid
-import ly.david.musicsearch.data.musicbrainz.auth.Logout
 import ly.david.musicsearch.data.musicbrainz.auth.MusicBrainzLoginActivityResultContract
 import ly.david.musicsearch.data.musicbrainz.api.MusicBrainzApi
 import ly.david.musicsearch.domain.collection.usecase.GetAllCollections
-import net.openid.appauth.AuthorizationResponse
 import org.koin.android.annotation.KoinViewModel
 import timber.log.Timber
 import java.util.UUID
@@ -38,8 +36,7 @@ internal class TopLevelViewModel(
     private val musicBrainzApi: MusicBrainzApi,
 
     private val musicBrainzLoginActivityResultContract: MusicBrainzLoginActivityResultContract,
-    private val loginUseCase: LoginAndroid,
-    private val logoutUseCase: Logout,
+    private val login: LoginAndroid,
 ) : ViewModel() {
 
     data class RemoteResult(
@@ -160,7 +157,7 @@ internal class TopLevelViewModel(
 
     fun login(result: MusicBrainzLoginActivityResultContract.Result) {
         viewModelScope.launch {
-            loginUseCase(result)
+            login(result)
         }
     }
 }
