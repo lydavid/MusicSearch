30a07e6f45a24733bb502e9c6d429a7c97595045
David Ly <22302001+lydavid@users.noreply.github.com>
2024-03-02 18:28:47 -0500

    multipreview screen sizes and light/dark

--- shared/feature/collections/src/androidMain/kotlin/ly/david/musicsearch/shared/feature/collections/list/CollectionListPreview.kt
+++ shared/feature/collections/src/androidMain/kotlin/ly/david/musicsearch/shared/feature/collections/list/CollectionListPreview.kt
@@ -2,17 +2,15 @@ package ly.david.musicsearch.shared.feature.collections.list
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.paging.PagingData
 import app.cash.paging.compose.collectAsLazyPagingItems
 import kotlinx.coroutines.flow.MutableStateFlow
 import ly.david.musicsearch.core.models.listitem.CollectionListItemModel
 import ly.david.musicsearch.core.models.network.MusicBrainzEntity
+import ly.david.ui.core.preview.DefaultPreviews
 import ly.david.ui.core.theme.PreviewTheme
 
-@PreviewLightDark
-@PreviewScreenSizes
+@DefaultPreviews
 @Composable
 internal fun PreviewCollectionList() {
     PreviewTheme {
