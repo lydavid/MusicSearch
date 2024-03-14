b875d43aa24535b4bd9797b7d769a2b2fa866e24
David Ly <22302001+lydavid@users.noreply.github.com>
2024-03-14 18:32:11 -0400

    remove TopLevelScaffold

--- android/app/src/androidTest/java/ly/david/mbjc/LookupEachEntityErrorTest.kt
+++ android/app/src/androidTest/java/ly/david/mbjc/LookupEachEntityErrorTest.kt
@@ -1,69 +1,52 @@
 package ly.david.mbjc
 
-import androidx.activity.compose.setContent
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.slack.circuit.foundation.Circuit
-import com.slack.circuit.foundation.CircuitCompositionLocals
-import kotlinx.coroutines.test.runTest
-import ly.david.mbjc.ui.TopLevelScaffold
-import ly.david.mbjc.ui.navigation.goToEntityScreen
-import ly.david.musicsearch.core.models.network.MusicBrainzEntity
-import ly.david.musicsearch.strings.AppStrings
-import ly.david.ui.core.theme.PreviewTheme
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.koin.test.inject
-
-@RunWith(Parameterized::class)
-internal class LookupEachEntityErrorTest(
-    private val entity: MusicBrainzEntity,
-) : MainActivityTest() {
-
-    companion object {
-        @JvmStatic
-        @Parameterized.Parameters(name = "{0}")
-        fun data(): Collection<MusicBrainzEntity> {
-            return MusicBrainzEntity.values().filterNot {
-                it in listOf(
-                    MusicBrainzEntity.URL,
-                    MusicBrainzEntity.COLLECTION,
-                )
-            }
-        }
-    }
-
-    private val strings: AppStrings by inject()
-    private val circuit: Circuit by inject()
-    private lateinit var navController: NavHostController
-
-    @Before
-    fun setupApp() {
-        composeTestRule.activity.setContent {
-            navController = rememberNavController()
-            PreviewTheme {
-                CircuitCompositionLocals(circuit) {
-                    TopLevelScaffold(navController)
-                }
-            }
-        }
-    }
-
-    @Test
-    fun lookupEachEntityError() = runTest {
-        composeTestRule.awaitIdle()
-
-        val entityId = "error"
-        navController.goToEntityScreen(
-            entity = entity,
-            id = entityId,
-        )
-
-        waitForThenAssertAtLeastOneIsDisplayed(strings.retry)
-
-        // TODO: in order to make retry actually work, we need to be able to fake out error
-        //  rather than pass an id that results in error
-    }
-}
+// @RunWith(Parameterized::class)
+// internal class LookupEachEntityErrorTest(
+//    private val entity: MusicBrainzEntity,
+// ) : MainActivityTest() {
+//
+//    companion object {
+//        @JvmStatic
+//        @Parameterized.Parameters(name = "{0}")
+//        fun data(): Collection<MusicBrainzEntity> {
+//            return MusicBrainzEntity.values().filterNot {
+//                it in listOf(
+//                    MusicBrainzEntity.URL,
+//                    MusicBrainzEntity.COLLECTION,
+//                )
+//            }
+//        }
+//    }
+//
+//    private val strings: AppStrings by inject()
+//    private val circuit: Circuit by inject()
+//    private lateinit var navController: NavHostController
+//
+//    @Before
+//    fun setupApp() {
+//        composeTestRule.activity.setContent {
+//            navController = rememberNavController()
+//            PreviewTheme {
+//                CircuitCompositionLocals(circuit) {
+//                    TopLevelScaffold(navController)
+//                }
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun lookupEachEntityError() = runTest {
+//        composeTestRule.awaitIdle()
+//
+//        val entityId = "error"
+//        navController.goToEntityScreen(
+//            entity = entity,
+//            id = entityId,
+//        )
+//
+//        waitForThenAssertAtLeastOneIsDisplayed(strings.retry)
+//
+//        // TODO: in order to make retry actually work, we need to be able to fake out error
+//        //  rather than pass an id that results in error
+//    }
+// }
--- android/app/src/main/java/ly/david/mbjc/ui/navigation/NavigationGraph.kt
+++ android/app/src/main/java/ly/david/mbjc/ui/navigation/NavigationGraph.kt
@@ -2,42 +2,17 @@ package ly.david.mbjc.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import androidx.navigation.navDeepLink
-import com.slack.circuit.backstack.rememberSaveableBackStack
-import com.slack.circuit.foundation.CircuitContent
-import com.slack.circuit.foundation.NavEvent
-import com.slack.circuit.foundation.NavigableCircuitContent
-import com.slack.circuit.foundation.rememberCircuitNavigator
-import ly.david.mbjc.DEEP_LINK_SCHEMA
 import ly.david.musicsearch.android.feature.nowplaying.NowPlayingHistoryScaffold
 import ly.david.musicsearch.android.feature.spotify.SpotifyScaffold
-import ly.david.musicsearch.core.models.common.transformThisIfNotNullOrEmpty
 import ly.david.musicsearch.core.models.navigation.Destination
-import ly.david.musicsearch.core.models.navigation.toLookupDestination
 import ly.david.musicsearch.core.models.network.MusicBrainzEntity
 import ly.david.musicsearch.core.models.network.resourceUri
-import ly.david.musicsearch.core.models.network.toMusicBrainzEntity
-import ly.david.ui.common.screen.CollectionListScreen
-import ly.david.ui.common.screen.CollectionScreen
-import ly.david.ui.common.screen.DetailsScreen
-import ly.david.ui.common.screen.HistoryScreen
-import ly.david.ui.common.screen.NowPlayingHistoryScreen
-import ly.david.ui.common.screen.SearchScreen
-import ly.david.ui.common.screen.SettingsScreen
-import ly.david.ui.common.screen.SpotifyPlayingScreen
-import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
-private const val ID = "id"
-private const val TITLE = "title"
 private const val QUERY = "query"
 private const val TYPE = "type"
 
@@ -49,78 +24,20 @@ private fun String.encodeUtf8(): String {
     )
 }
 
-private fun String.decodeUtf8(): String {
-    return URLDecoder.decode(
-        this,
-        StandardCharsets.UTF_8.toString(),
-    )
-}
-
-internal fun NavHostController.goToEntityScreen(
-    entity: MusicBrainzEntity,
-    id: String,
-    title: String? = null,
-) {
-    val route = "${entity.toLookupDestination().route}/$id" +
-        title?.encodeUtf8().transformThisIfNotNullOrEmpty { "?$TITLE=$it" }
-    this.navigate(route)
-}
-
 @Composable
 internal fun NavigationGraph(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    val uriPrefix = "$DEEP_LINK_SCHEMA://app/"
-
     NavHost(
         navController = navController,
         startDestination = Destination.LOOKUP.route,
     ) {
-
         val searchMusicBrainz: (String, MusicBrainzEntity) -> Unit = { query, type ->
             val route = Destination.LOOKUP.route +
                 "?$QUERY=${query.encodeUtf8()}" +
                 "&$TYPE=${type.resourceUri}"
             navController.navigate(route)
-        }
-
-        composable(
-            Destination.SETTINGS.route,
-            deepLinks = listOf(
-                navDeepLink {
-                    uriPattern = "$uriPrefix${Destination.SETTINGS.route}"
-                },
-            ),
-        ) {
-            val backStack = rememberSaveableBackStack(root = SettingsScreen)
-            val navigator = rememberCircuitNavigator(backStack)
-            NavigableCircuitContent(
-                navigator = navigator,
-                backStack = backStack,
-                modifier = modifier,
-                unavailableRoute = { screen, nestedModifier ->
-                    when (screen) {
-                        is NowPlayingHistoryScreen -> {
-                            NowPlayingHistoryScaffold(
-                                modifier = nestedModifier,
-                                onBack = navigator::pop,
-                                searchMusicBrainz = searchMusicBrainz,
-                            )
-                        }
-
-                        is SpotifyPlayingScreen -> {
-                            SpotifyScaffold(
-                                modifier = nestedModifier,
-                                onBack = navigator::pop,
-                                searchMusicBrainz = searchMusicBrainz,
-                            )
-                        }
-
-                        else -> {}
-                    }
-                },
-            )
         }
 
         composable(
@@ -142,37 +59,5 @@ internal fun NavigationGraph(
                 searchMusicBrainz = searchMusicBrainz,
             )
         }
-    }
-}
-
-private fun NavGraphBuilder.addLookupEntityScreen(
-    entity: MusicBrainzEntity,
-    uriPrefix: String,
-    scaffold: @Composable (entityId: String, titleWithDisambiguation: String?) -> Unit,
-) {
-    composable(
-        route = "${entity.toLookupDestination().route}/{$ID}?$TITLE={$TITLE}",
-        arguments = listOf(
-            navArgument(ID) {
-                type = NavType.StringType // Make argument type safe
-            },
-            navArgument(TITLE) {
-                nullable = true
-                defaultValue = null
-                type = NavType.StringType
-            },
-        ),
-        deepLinks = listOf(
-            navDeepLink {
-                uriPattern = "$uriPrefix${entity.resourceUri}/{$ID}?$TITLE={$TITLE}"
-            },
-        ),
-    ) { entry: NavBackStackEntry ->
-        val entityId = entry.arguments?.getString(ID) ?: return@composable
-        val title = entry.arguments?.getString(TITLE)?.decodeUtf8()
-        scaffold(
-            entityId,
-            title,
-        )
     }
 }
