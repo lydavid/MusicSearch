a5b8f8b7ab74a633acd83aea5231fb081b3c22e6
David Ly <22302001+lydavid@users.noreply.github.com>
2024-04-04 22:52:54 -0400

    ios app that can run but errors

---
+++ ios-app/musicsearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  musicsearch
+//
+//  Created by David Ly on 2024-04-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app/musicsearch/musicsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicsearchApp.swift
+//  musicsearch
+//
+//  Created by David Ly on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct musicsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
