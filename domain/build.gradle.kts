4552d4dc30a35870c5eee4f7be5bcbb9064468ee
David Ly <22302001+lydavid@users.noreply.github.com>
2024-04-04 22:43:18 -0400

    chore: add missing coroutines core dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
                 implementation(libs.koin.annotations)
                 implementation(libs.koin.core)
                 implementation(libs.kotlinx.collections.immutable)
+                implementation(libs.kotlinx.coroutines.core)
                 implementation(libs.paging.common)
             }
         }
