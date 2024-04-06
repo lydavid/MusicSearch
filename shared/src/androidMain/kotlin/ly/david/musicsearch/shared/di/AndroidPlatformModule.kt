2573f69f2d99c77916d264dea6f8bd110b30078a
David Ly <22302001+lydavid@users.noreply.github.com>
2024-04-06 16:08:42 -0400

    chore: generate ksp commonMain for :domain

--- shared/src/androidMain/kotlin/ly/david/musicsearch/shared/di/AndroidPlatformModule.kt
+++ shared/src/androidMain/kotlin/ly/david/musicsearch/shared/di/AndroidPlatformModule.kt
@@ -2,18 +2,14 @@ package ly.david.musicsearch.shared.di
 
 import ly.david.musicsearch.android.feature.nowplaying.nowPlayingFeatureModule
 import ly.david.musicsearch.android.feature.spotify.spotifyFeatureModule
-import ly.david.musicsearch.domain.DomainModule
 import ly.david.musicsearch.ui.image.di.imageModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.koin.ksp.generated.module
 
 actual val platformModule: Module = module {
     includes(
         nowPlayingFeatureModule,
         spotifyFeatureModule,
         imageModule,
-
-        DomainModule().module,
     )
 }
