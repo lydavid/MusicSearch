370597e19356de217ccb1d75e86cfecbcd9b3e8c
David Ly <22302001+lydavid@users.noreply.github.com>
2024-04-06 16:25:39 -0400

    chore: revert "chore: generate ksp commonMain for :domain"
    This reverts commit 2573f69f2d99c77916d264dea6f8bd110b30078a.

--- shared/src/jvmMain/kotlin/ly/david/musicsearch/shared/di/DesktopPlatformModule.kt
+++ shared/src/jvmMain/kotlin/ly/david/musicsearch/shared/di/DesktopPlatformModule.kt
@@ -1,8 +1,12 @@
 package ly.david.musicsearch.shared.di
 
+import ly.david.musicsearch.domain.DomainModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.koin.ksp.generated.module
 
 actual val platformModule: Module = module {
-    // no-op
+    includes(
+        DomainModule().module,
+    )
 }
