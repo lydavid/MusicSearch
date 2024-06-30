eef16353e1922cbf148dde1ee7ced7d9e97c9339
David Ly <22302001+lydavid@users.noreply.github.com>
2024-06-29 23:55:20 -0400

    chore: remove todo

--- core/models/src/commonMain/kotlin/ly/david/musicsearch/core/models/listitem/WorkListItemModel.kt
+++ core/models/src/commonMain/kotlin/ly/david/musicsearch/core/models/listitem/WorkListItemModel.kt
@@ -3,7 +3,6 @@ package ly.david.musicsearch.core.models.listitem
 import ly.david.musicsearch.core.models.work.Work
 import ly.david.musicsearch.core.models.work.WorkAttributeUiModel
 
-// TODO: map "qaa" to Artificial (Other), and rest from 3 letter code to full language name
 data class WorkListItemModel(
     override val id: String,
     override val name: String,
