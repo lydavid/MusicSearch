4ede378c7b527f311346b6d08eb972810fa4b6a6
David Ly <22302001+lydavid@users.noreply.github.com>
2024-06-30 13:56:12 -0400

    chore: fix Compose UI preview missing from Android Studio
    fixes #980

--- build-logic/convention/src/main/kotlin/ly/david/convention/plugin/ComposeMultiplatformConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/ly/david/convention/plugin/ComposeMultiplatformConventionPlugin.kt
@@ -1,7 +1,9 @@
 package ly.david.convention.plugin
 
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
 @Suppress("unused")
 class ComposeMultiplatformConventionPlugin : Plugin<Project> {
@@ -11,6 +13,19 @@ class ComposeMultiplatformConventionPlugin : Plugin<Project> {
                 apply("org.jetbrains.compose")
                 apply("org.jetbrains.kotlin.plugin.compose")
             }
+            if (pluginManager.hasPlugin("ly.david.android.library")) {
+                android {
+                    buildFeatures {
+                        compose = true
+                    }
+                }
+            }
         }
     }
 }
+
+private fun Project.android(configure: CommonExtension<*, *, *, *, *, *>.() -> Unit) =
+    extensions.configure(
+        CommonExtension::class,
+        configure,
+    )
