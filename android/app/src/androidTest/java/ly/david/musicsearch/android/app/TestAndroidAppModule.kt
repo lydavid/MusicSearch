b160b9b773458cae64ccd496378ea0ecc2f3b7c8
David Ly <22302001+lydavid@users.noreply.github.com>
2024-07-10 23:01:16 -0400

    chore: remove FakeMusicBrainzApi which we couldn't change easily per-test

--- android/app/src/androidTest/java/ly/david/musicsearch/android/app/TestAndroidAppModule.kt
+++ android/app/src/androidTest/java/ly/david/musicsearch/android/app/TestAndroidAppModule.kt
@@ -1,6 +1,5 @@
 package ly.david.musicsearch.android.app
 
-import ly.david.data.test.di.testApiModule
 import ly.david.musicsearch.android.app.di.testPreferencesDataStoreModule
 import ly.david.musicsearch.shared.di.sharedModule
 import org.koin.dsl.module
@@ -9,7 +8,6 @@ val testAndroidAppModule = module {
     includes(
         sharedModule,
         androidAppModule,
-        testApiModule,
         testPreferencesDataStoreModule,
     )
 }
--- data/repository/src/jvmTest/kotlin/ly/david/musicsearch/data/repository/SearchMusicBrainzPagingSourceTest.kt
+++ data/repository/src/jvmTest/kotlin/ly/david/musicsearch/data/repository/SearchMusicBrainzPagingSourceTest.kt
@@ -2,10 +2,32 @@ package ly.david.musicsearch.data.repository
 
 import androidx.paging.PagingSource
 import kotlinx.coroutines.test.runTest
-import ly.david.data.test.api.FakeMusicBrainzApi
+import ly.david.data.test.searchAreasResponse
+import ly.david.data.test.searchArtistsResponse
+import ly.david.data.test.searchEventsResponse
+import ly.david.data.test.searchInstrumentsResponse
+import ly.david.data.test.searchLabelsResponse
+import ly.david.data.test.searchPlacesResponse
+import ly.david.data.test.searchRecordingsResponse
+import ly.david.data.test.searchReleaseGroupsResponse
+import ly.david.data.test.searchReleasesResponse
+import ly.david.data.test.searchSeriesResponse
+import ly.david.data.test.searchWorksResponse
 import ly.david.data.test.toFakeMusicBrainzModel
 import ly.david.musicsearch.core.models.network.MusicBrainzEntity
 import ly.david.musicsearch.core.models.network.searchableEntities
+import ly.david.musicsearch.data.musicbrainz.api.SearchApi
+import ly.david.musicsearch.data.musicbrainz.api.SearchAreasResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchArtistsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchEventsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchInstrumentsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchLabelsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchPlacesResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchRecordingsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchReleaseGroupsResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchReleasesResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchSeriesResponse
+import ly.david.musicsearch.data.musicbrainz.api.SearchWorksResponse
 import ly.david.musicsearch.data.repository.internal.paging.SearchMusicBrainzPagingSource
 import ly.david.musicsearch.data.repository.internal.paging.toListItemModel
 import org.junit.Assert.assertEquals
@@ -16,10 +38,102 @@ import org.junit.runners.Parameterized
 @RunWith(Parameterized::class)
 internal class SearchMusicBrainzPagingSourceTest(private val entity: MusicBrainzEntity) {
 
+    private class FakeSearchApi : SearchApi {
+        // region Search
+        override suspend fun queryAreas(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchAreasResponse {
+            return searchAreasResponse
+        }
+
+        override suspend fun queryArtists(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchArtistsResponse {
+            return searchArtistsResponse
+        }
+
+        override suspend fun queryEvents(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchEventsResponse {
+            return searchEventsResponse
+        }
+
+        override suspend fun queryInstruments(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchInstrumentsResponse {
+            return searchInstrumentsResponse
+        }
+
+        override suspend fun queryLabels(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchLabelsResponse {
+            return searchLabelsResponse
+        }
+
+        override suspend fun queryPlaces(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchPlacesResponse {
+            return searchPlacesResponse
+        }
+
+        override suspend fun queryRecordings(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchRecordingsResponse {
+            return searchRecordingsResponse
+        }
+
+        override suspend fun queryReleases(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchReleasesResponse {
+            return searchReleasesResponse
+        }
+
+        override suspend fun queryReleaseGroups(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchReleaseGroupsResponse {
+            return searchReleaseGroupsResponse
+        }
+
+        override suspend fun querySeries(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchSeriesResponse {
+            return searchSeriesResponse
+        }
+
+        override suspend fun queryWorks(
+            query: String,
+            limit: Int,
+            offset: Int,
+        ): SearchWorksResponse {
+            return searchWorksResponse
+        }
+        // endregion
+    }
+
     @Test
     fun loadEachEntity() = runTest {
         val pagingSource = SearchMusicBrainzPagingSource(
-            FakeMusicBrainzApi(),
+            FakeSearchApi(),
             entity,
             "",
         )
