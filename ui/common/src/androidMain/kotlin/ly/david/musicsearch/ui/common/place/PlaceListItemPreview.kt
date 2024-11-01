92b8ffcf1a706e68bd40237d5de7a51d65e591eb
David Ly <22302001+lydavid@users.noreply.github.com>
2024-10-31 22:10:09 -0400

    feat: bold places that have not been clicked on
    #1158

--- ui/common/src/androidMain/kotlin/ly/david/musicsearch/ui/common/place/PlaceListItemPreview.kt
+++ ui/common/src/androidMain/kotlin/ly/david/musicsearch/ui/common/place/PlaceListItemPreview.kt
@@ -2,50 +2,70 @@ package ly.david.musicsearch.ui.common.place
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import ly.david.musicsearch.shared.domain.LifeSpanUiModel
 import ly.david.musicsearch.shared.domain.listitem.PlaceListItemModel
-import ly.david.musicsearch.ui.core.preview.DefaultPreviews
 import ly.david.musicsearch.ui.core.theme.PreviewTheme
 
-internal class PlacePreviewParameterProvider : PreviewParameterProvider<PlaceListItemModel> {
-    override val values = sequenceOf(
-        PlaceListItemModel(
-            id = "2",
-            name = "Place Name",
-            address = "123 Fake St",
-        ),
-        PlaceListItemModel(
-            id = "ed121457-87f6-4df9-a24b-d3f1bab1fdad",
-            name = "Sony Music Studios",
-            disambiguation = "NYC, closed 2007",
-            type = "Studio",
-            address = "460 W. 54th St., at 10th Avenue, Manhatten, NY",
-            lifeSpan = LifeSpanUiModel(
-                begin = "1993",
-                end = "2007-08",
-                ended = true,
-            ),
-        ),
-        PlaceListItemModel(
-            id = "4d43b9d8-162d-4ac5-8068-dfb009722484",
-            name = "日本武道館",
-            type = "Indoor arena",
-            address = "〒102-8321 東京都千代田区北の丸公園2-3",
-            lifeSpan = LifeSpanUiModel(begin = "1964-10-03"),
-        ),
-    )
-}
-
-@DefaultPreviews
+@PreviewLightDark
 @Composable
-private fun Preview(
-    @PreviewParameter(PlacePreviewParameterProvider::class) place: PlaceListItemModel,
-) {
+internal fun PreviewPlaceListItem() {
     PreviewTheme {
         Surface {
-            PlaceListItem(place)
+            PlaceListItem(
+                place = PlaceListItemModel(
+                    id = "2",
+                    name = "Place Name",
+                    address = "123 Fake St",
+                ),
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+internal fun PreviewPlaceListItemAllInfo() {
+    PreviewTheme {
+        Surface {
+            PlaceListItem(
+                place = PlaceListItemModel(
+                    id = "ed121457-87f6-4df9-a24b-d3f1bab1fdad",
+                    name = "Sony Music Studios",
+                    disambiguation = "NYC, closed 2007",
+                    type = "Studio",
+                    address = "460 W. 54th St., at 10th Avenue, Manhatten, NY",
+                    lifeSpan = LifeSpanUiModel(
+                        begin = "1993",
+                        end = "2007-08",
+                        ended = true,
+                    ),
+                ),
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+internal fun PreviewPlaceListItemVisited() {
+    PreviewTheme {
+        Surface {
+            PlaceListItem(
+                place = PlaceListItemModel(
+                    id = "ed121457-87f6-4df9-a24b-d3f1bab1fdad",
+                    name = "Sony Music Studios",
+                    disambiguation = "NYC, closed 2007",
+                    type = "Studio",
+                    address = "460 W. 54th St., at 10th Avenue, Manhatten, NY",
+                    lifeSpan = LifeSpanUiModel(
+                        begin = "1993",
+                        end = "2007-08",
+                        ended = true,
+                    ),
+                    visited = true,
+                ),
+            )
         }
     }
 }
--- ui/common/src/androidUnitTest/kotlin/ly/david/musicsearch/ui/common/place/PlaceListItemTest.kt
+++ ui/common/src/androidUnitTest/kotlin/ly/david/musicsearch/ui/common/place/PlaceListItemTest.kt
@@ -1,45 +1,28 @@
 package ly.david.musicsearch.ui.common.place
 
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import ly.david.musicsearch.shared.domain.LifeSpanUiModel
-import ly.david.musicsearch.shared.domain.listitem.PlaceListItemModel
 import ly.david.musicsearch.ui.test.screenshot.ScreenshotTest
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(TestParameterInjector::class)
 class PlaceListItemTest : ScreenshotTest() {
 
     @Test
     fun simple() {
         snapshot {
-            PlaceListItem(
-                place = PlaceListItemModel(
-                    id = "2",
-                    name = "Place Name",
-                    address = "123 Fake St",
-                ),
-            )
+            PreviewPlaceListItem()
         }
     }
 
     @Test
     fun allInfo() {
         snapshot {
-            PlaceListItem(
-                place = PlaceListItemModel(
-                    id = "ed121457-87f6-4df9-a24b-d3f1bab1fdad",
-                    name = "Sony Music Studios",
-                    disambiguation = "NYC, closed 2007",
-                    type = "Studio",
-                    address = "460 W. 54th St., at 10th Avenue, Manhatten, NY",
-                    lifeSpan = LifeSpanUiModel(
-                        begin = "1993",
-                        end = "2007-08",
-                        ended = true,
-                    ),
-                ),
-            )
+            PreviewPlaceListItemAllInfo()
+        }
+    }
+
+    @Test
+    fun visited() {
+        snapshot {
+            PreviewPlaceListItemVisited()
         }
     }
 }
