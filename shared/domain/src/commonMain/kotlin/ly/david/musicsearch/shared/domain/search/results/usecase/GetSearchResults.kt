43f9dae1b6a43bde3cc69c7bb0dc222e9e22cbad
David Ly <22302001+lydavid@users.noreply.github.com>
2024-11-07 22:19:24 -0500

    chore: debounce flow

--- shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/search/results/usecase/GetSearchResults.kt
+++ shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/search/results/usecase/GetSearchResults.kt
@@ -3,13 +3,18 @@ package ly.david.musicsearch.shared.domain.search.results.usecase
 import androidx.paging.PagingData
 import app.cash.paging.cachedIn
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
 import ly.david.musicsearch.shared.domain.listitem.ListItemModel
 import ly.david.musicsearch.shared.domain.network.MusicBrainzEntity
 import ly.david.musicsearch.shared.domain.search.results.SearchResultsRepository
 
+private const val SEARCH_DELAY_MS = 500L
+
+@OptIn(FlowPreview::class)
 class GetSearchResults(
     private val searchResultsRepository: SearchResultsRepository,
     private val coroutineScope: CoroutineScope,
@@ -24,6 +29,7 @@ class GetSearchResults(
                 entity = entity,
                 query = query,
             )
+                .debounce(SEARCH_DELAY_MS)
                 .distinctUntilChanged()
                 .cachedIn(scope = coroutineScope)
         }
