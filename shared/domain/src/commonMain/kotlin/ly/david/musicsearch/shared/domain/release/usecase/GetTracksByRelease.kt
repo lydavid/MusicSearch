43ac4d2c5bc227eec9f6ee55eff509042ab2bef5
David Ly <22302001+lydavid@users.noreply.github.com>
2024-11-09 10:05:19 -0500

    fix: retain tracks when returning from back stack
    #1197

--- shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/release/usecase/GetTracksByRelease.kt
+++ shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/release/usecase/GetTracksByRelease.kt
@@ -1,18 +1,31 @@
 package ly.david.musicsearch.shared.domain.release.usecase
 
 import app.cash.paging.PagingData
+import app.cash.paging.cachedIn
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emptyFlow
 import ly.david.musicsearch.shared.domain.listitem.ListItemModel
 import ly.david.musicsearch.shared.domain.release.ReleaseRepository
 
 class GetTracksByRelease(
     private val releaseRepository: ReleaseRepository,
+    private val coroutineScope: CoroutineScope,
 ) {
     operator fun invoke(
         releaseId: String,
         query: String,
-    ): Flow<PagingData<ListItemModel>> = releaseRepository.observeTracksByRelease(
-        releaseId = releaseId,
-        query = query,
-    )
+    ): Flow<PagingData<ListItemModel>> {
+        return if (releaseId.isEmpty()) {
+            emptyFlow()
+        } else {
+            releaseRepository.observeTracksByRelease(
+                releaseId = releaseId,
+                query = query,
+            )
+                .distinctUntilChanged()
+                .cachedIn(scope = coroutineScope)
+        }
+    }
 }
