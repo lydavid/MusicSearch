209e753f28bd8006ac890768b59fe4c23a6a7426
David Ly <22302001+lydavid@users.noreply.github.com>
2024-11-08 21:14:49 -0500

    fix: retain labels when returning from back stack
    #1197

--- shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/label/usecase/GetLabelsByEntity.kt
+++ shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/label/usecase/GetLabelsByEntity.kt
@@ -1,7 +1,10 @@
 package ly.david.musicsearch.shared.domain.label.usecase
 
 import androidx.paging.PagingData
+import app.cash.paging.cachedIn
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
 import ly.david.musicsearch.shared.domain.ListFilters
 import ly.david.musicsearch.shared.domain.base.usecase.GetEntitiesByEntity
@@ -11,6 +14,7 @@ import ly.david.musicsearch.shared.domain.network.MusicBrainzEntity
 
 class GetLabelsByEntity(
     private val labelsByEntityRepository: LabelsByEntityRepository,
+    private val coroutineScope: CoroutineScope,
 ) : GetEntitiesByEntity<LabelListItemModel> {
     override operator fun invoke(
         entityId: String,
@@ -24,6 +28,8 @@ class GetLabelsByEntity(
                 entity = entity,
                 listFilters = listFilters,
             )
+                .distinctUntilChanged()
+                .cachedIn(scope = coroutineScope)
         }
     }
 }
--- ui/common/src/commonMain/kotlin/ly/david/musicsearch/ui/common/label/LabelsByEntityPresenter.kt
+++ ui/common/src/commonMain/kotlin/ly/david/musicsearch/ui/common/label/LabelsByEntityPresenter.kt
@@ -3,55 +3,46 @@ package ly.david.musicsearch.ui.common.label
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import app.cash.paging.PagingData
 import app.cash.paging.compose.LazyPagingItems
 import app.cash.paging.compose.collectAsLazyPagingItems
+import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.presenter.Presenter
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import ly.david.musicsearch.shared.domain.ListFilters
+import ly.david.musicsearch.shared.domain.label.usecase.GetLabelsByEntity
 import ly.david.musicsearch.shared.domain.listitem.LabelListItemModel
 import ly.david.musicsearch.shared.domain.network.MusicBrainzEntity
-import ly.david.musicsearch.shared.domain.label.usecase.GetLabelsByEntity
 
 class LabelsByEntityPresenter(
     private val getLabelsByEntity: GetLabelsByEntity,
 ) : Presenter<LabelsByEntityUiState> {
     @Composable
     override fun present(): LabelsByEntityUiState {
-        var query by rememberSaveable { mutableStateOf("") }
         var id: String by rememberSaveable { mutableStateOf("") }
-        var isRemote: Boolean by rememberSaveable { mutableStateOf(false) }
         var entity: MusicBrainzEntity? by rememberSaveable { mutableStateOf(null) }
-        var labelListItems: Flow<PagingData<LabelListItemModel>> by remember { mutableStateOf(emptyFlow()) }
-        val lazyListState: LazyListState = rememberLazyListState()
-
-        LaunchedEffect(
-            key1 = id,
-            key2 = entity,
-            key3 = query,
-        ) {
-            if (id.isEmpty()) return@LaunchedEffect
-            val capturedEntity = entity ?: return@LaunchedEffect
-
-            labelListItems = getLabelsByEntity(
-                entityId = id,
-                entity = capturedEntity,
-                listFilters = ListFilters(
-                    query = query,
-                    isRemote = isRemote,
+        var query by rememberSaveable { mutableStateOf("") }
+        var isRemote: Boolean by rememberSaveable { mutableStateOf(false) }
+        val labelListItems: Flow<PagingData<LabelListItemModel>> by rememberRetained(id, entity, query) {
+            mutableStateOf(
+                getLabelsByEntity(
+                    entityId = id,
+                    entity = entity,
+                    listFilters = ListFilters(
+                        query = query,
+                        isRemote = isRemote,
+                    ),
                 ),
             )
         }
+        val lazyListState: LazyListState = rememberLazyListState()
 
         fun eventSink(event: LabelsByEntityUiEvent) {
             when (event) {
