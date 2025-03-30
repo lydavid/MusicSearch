7d81a0aeadcdcc46c353e9f1c37d57493f8d371f
David Ly <22302001+lydavid@users.noreply.github.com>
2025-03-29 22:21:21 -0400

    fix: show release group first release date (#1420)

--- shared/feature/details/src/androidMain/kotlin/ly/david/musicsearch/shared/feature/details/releasegroup/PreviewReleaseGroupDetailsUi.kt
+++ shared/feature/details/src/androidMain/kotlin/ly/david/musicsearch/shared/feature/details/releasegroup/PreviewReleaseGroupDetailsUi.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import ly.david.musicsearch.shared.domain.releasegroup.ReleaseGroupDetailsModel
+import ly.david.musicsearch.shared.domain.wikimedia.WikipediaExtract
 import ly.david.musicsearch.ui.core.theme.PreviewTheme
 
 @PreviewLightDark
@@ -13,9 +14,14 @@ internal fun PreviewReleaseGroupDetailsUi() {
         Surface {
             ReleaseGroupDetailsUi(
                 releaseGroup = ReleaseGroupDetailsModel(
-                    id = "0a7f8b50-7888-4c50-9548-80144dad3460",
-                    name = "夏枯れ",
+                    id = "bdaeec2d-94f1-46b5-91f3-340ec6939c66",
+                    name = "Under Pressure",
                     primaryType = "Single",
+                    firstReleaseDate = "1981-10",
+                    wikipediaExtract = WikipediaExtract(
+                        extract = "\"Under Pressure\" is a song by the British rock band Queen and singer David Bowie.",
+                        wikipediaUrl = "https://en.wikipedia.org/wiki/Under_Pressure",
+                    ),
                 ),
             )
         }
