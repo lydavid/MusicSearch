b5404f543b4d5910b407b1f481af4dc8dd425e1f
David Ly <22302001+lydavid@users.noreply.github.com>
2025-05-01 20:44:22 -0400

    fix: apply debounce to search input instead of results to make sure only the most recent results are shown
    this should fix the flake in our e2e test
    closes #1419

--- shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/search/results/usecase/GetSearchResults.kt
+++ shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/search/results/usecase/GetSearchResults.kt
@@ -3,35 +3,45 @@ package ly.david.musicsearch.shared.domain.search.results.usecase
 import androidx.paging.PagingData
 import app.cash.paging.cachedIn
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import ly.david.musicsearch.shared.domain.listitem.ListItemModel
 import ly.david.musicsearch.shared.domain.network.MusicBrainzEntity
 import ly.david.musicsearch.shared.domain.search.results.SearchResultsRepository
 
 private const val SEARCH_DELAY_MS = 500L
 
-@OptIn(FlowPreview::class)
+@OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
 class GetSearchResults(
     private val searchResultsRepository: SearchResultsRepository,
     private val coroutineScope: CoroutineScope,
 ) {
+    private val _searchQueries = MutableStateFlow(MusicBrainzEntity.ARTIST to "")
+
     operator fun invoke(
         entity: MusicBrainzEntity,
         query: String,
     ): Flow<PagingData<ListItemModel>> {
-        return when {
-            query.isBlank() -> emptyFlow()
-            else -> searchResultsRepository.observeSearchResults(
-                entity = entity,
-                query = query,
-            )
-                .debounce(SEARCH_DELAY_MS)
-                .distinctUntilChanged()
-                .cachedIn(scope = coroutineScope)
-        }
+        _searchQueries.value = entity to query
+
+        return _searchQueries
+            .debounce(SEARCH_DELAY_MS)
+            .distinctUntilChanged()
+            .flatMapLatest { (currentEntity, currentQuery) ->
+                when {
+                    currentQuery.isBlank() -> emptyFlow()
+                    else -> searchResultsRepository.observeSearchResults(
+                        entity = currentEntity,
+                        query = currentQuery,
+                    )
+                }
+            }
+            .cachedIn(scope = coroutineScope)
     }
 }
