4fc2ef787c431b92260e1528120ec7a6d068ef16
David Ly <22302001+lydavid@users.noreply.github.com>
2025-05-04 21:33:58 -0400

    r

---
+++ shared/feature/details/src/androidUnitTest/kotlin/ly/david/musicsearch/shared/feature/details/release/ReleaseUiScreenshotTest.kt
@@ -0,0 +1,14 @@
+package ly.david.musicsearch.shared.feature.details.release
+
+import ly.david.musicsearch.ui.test.screenshot.ScreenshotTest
+import org.junit.Test
+
+class ReleaseUiScreenshotTest : ScreenshotTest() {
+
+    @Test
+    fun details() {
+        snapshot {
+            PreviewReleaseDetailsUi()
+        }
+    }
+}
