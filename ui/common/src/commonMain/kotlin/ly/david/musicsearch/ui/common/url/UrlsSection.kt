0c6c9b11bb4fabc92bbb3940b057fc18160d137f
David Ly <22302001+lydavid@users.noreply.github.com>
2025-05-11 19:50:51 -0400

    fix: hide external links header when there are none or all have been filtered out

--- ui/common/src/commonMain/kotlin/ly/david/musicsearch/ui/common/url/UrlsSection.kt
+++ ui/common/src/commonMain/kotlin/ly/david/musicsearch/ui/common/url/UrlsSection.kt
@@ -2,6 +2,7 @@ package ly.david.musicsearch.ui.common.url
 
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import ly.david.musicsearch.shared.domain.common.ifNotNullOrEmpty
 import ly.david.musicsearch.shared.domain.listitem.RelationListItemModel
 import ly.david.musicsearch.ui.common.listitem.ListSeparatorHeader
 import ly.david.musicsearch.ui.common.relation.UrlListItem
@@ -9,8 +10,10 @@ import ly.david.musicsearch.ui.common.relation.UrlListItem
 fun LazyListScope.urlsSection(
     urls: List<RelationListItemModel>,
 ) {
-    item {
-        ListSeparatorHeader("External links")
+    urls.ifNotNullOrEmpty {
+        item {
+            ListSeparatorHeader("External links")
+        }
     }
     items(urls) {
         UrlListItem(
