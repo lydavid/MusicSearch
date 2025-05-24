5cb4ee929517b3e7fc966a0aa5b1b7d50631e45f
David Ly <22302001+lydavid@users.noreply.github.com>
2025-05-24 19:29:36 -0400

    fix(ios): handle common exceptions when making API calls and retry request up to 3 times

--- data/common/network/src/iosMain/kotlin/ly/david/musicsearch/data/common/network/di/HttpClientModule.ios.kt
+++ data/common/network/src/iosMain/kotlin/ly/david/musicsearch/data/common/network/di/HttpClientModule.ios.kt
@@ -1,8 +1,8 @@
 package ly.david.musicsearch.data.common.network.di
 
 import io.ktor.client.engine.darwin.Darwin
-import io.ktor.client.engine.darwin.DarwinClientEngineConfig
 import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpResponseValidator
 import ly.david.musicsearch.data.common.network.ApiHttpClient
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -10,10 +10,18 @@ import org.koin.dsl.module
 actual val HttpClientModule: Module = module {
     single {
         ApiHttpClient.configAndCreate(engine = Darwin.create()) {
-//            install(HttpRequestRetry) {
-//                retryOnExceptionOrServerErrors(maxRetries = 3)
-//                exponentialDelay()
-//            }
+            HttpResponseValidator {
+                handleResponseExceptionWithRequest { exception, _ ->
+                    handleRecoverableException(
+                        logger = get(),
+                        exception = exception,
+                    )
+                }
+            }
+            install(HttpRequestRetry) {
+                retryOnExceptionOrServerErrors(maxRetries = 3)
+                exponentialDelay()
+            }
         }
     }
 }
