5de6c5325744d8e0bcd1abfc2d5ec813a98a882a
David Ly <22302001+lydavid@users.noreply.github.com>
2025-05-27 08:37:44 -0400

    fix: remove http cache so refreshing will get latest result from MusicBrainz
    closes #1550

--- data/common/network/src/androidMain/kotlin/ly/david/musicsearch/data/common/network/di/HttpClientModule.android.kt
+++ data/common/network/src/androidMain/kotlin/ly/david/musicsearch/data/common/network/di/HttpClientModule.android.kt
@@ -1,6 +1,5 @@
 package ly.david.musicsearch.data.common.network.di
 
-import android.content.Context
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import io.ktor.client.engine.okhttp.OkHttpConfig
 import io.ktor.client.engine.okhttp.OkHttpEngine
@@ -11,25 +10,15 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.HttpHeaders
 import ly.david.musicsearch.core.logging.Logger
 import ly.david.musicsearch.data.common.network.ApiHttpClient
-import okhttp3.Cache
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import java.io.File
 import io.ktor.client.plugins.logging.Logger as KtorLogger
 
 actual val HttpClientModule: Module = module {
     single {
         val okHttpConfig = OkHttpConfig()
         okHttpConfig.config {
-            cache(
-                cache = Cache(
-                    directory = File(
-                        get<Context>().cacheDir,
-                        "ktor_okhttp_cache",
-                    ),
-                    maxSize = 50 * 1024 * 1024,
-                ),
-            )
+            cache(null)
             addInterceptor(ChuckerInterceptor(context = get()))
         }
         ApiHttpClient.configAndCreate(
