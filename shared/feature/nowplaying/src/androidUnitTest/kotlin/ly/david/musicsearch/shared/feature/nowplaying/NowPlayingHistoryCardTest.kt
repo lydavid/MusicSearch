878051328c8b62336847a05869356234e40efb26
David Ly <22302001+lydavid@users.noreply.github.com>
2025-06-23 20:18:07 -0400

    f

--- shared/feature/nowplaying/src/androidUnitTest/kotlin/ly/david/musicsearch/shared/feature/nowplaying/NowPlayingHistoryCardTest.kt
+++ shared/feature/nowplaying/src/androidUnitTest/kotlin/ly/david/musicsearch/shared/feature/nowplaying/NowPlayingHistoryCardTest.kt
@@ -1,12 +1,8 @@
 package ly.david.musicsearch.shared.feature.nowplaying
 
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import ly.david.musicsearch.shared.feature.nowplaying.PreviewNowPlayingHistoryCard
 import ly.david.musicsearch.ui.test.screenshot.ScreenshotTest
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(TestParameterInjector::class)
 class NowPlayingHistoryCardTest : ScreenshotTest() {
 
     @Test
