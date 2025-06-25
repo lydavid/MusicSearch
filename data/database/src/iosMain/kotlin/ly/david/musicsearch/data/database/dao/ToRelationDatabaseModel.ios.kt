090f15659c9786a3ee0866653fccb97507ec276e
David Ly <22302001+lydavid@users.noreply.github.com>
2025-06-25 17:16:27 -0400

    fix(ios): decode urls in details

--- data/database/src/iosMain/kotlin/ly/david/musicsearch/data/database/dao/ToRelationDatabaseModel.ios.kt
+++ data/database/src/iosMain/kotlin/ly/david/musicsearch/data/database/dao/ToRelationDatabaseModel.ios.kt
@@ -1,6 +1,10 @@
 package ly.david.musicsearch.data.database.dao
 
+import platform.Foundation.NSString
+import platform.Foundation.stringByRemovingPercentEncoding
+
+// https://kotlinlang.org/docs/native-objc-interop.html#casting-between-mapped-types
+@Suppress("CAST_NEVER_SUCCEEDS")
 internal actual fun String.decodeUrl(): String {
-    // TODO: handle ios
-    return this
+    return (this as NSString).stringByRemovingPercentEncoding ?: this
 }
