52769b940766d4267dc72be78fee13b4726dc07b
David Ly <22302001+lydavid@users.noreply.github.com>
2025-07-17 22:09:50 -0400

    fix: catch UnknownHostException instead of crashing
    closes https://github.com/lydavid/MusicSearch/issues/124

--- data/common/network/src/jvmCommon/kotlin/ly/david/musicsearch/data/common/network/di/HandleRecoverablePlatformException.jvmCommon.kt
+++ data/common/network/src/jvmCommon/kotlin/ly/david/musicsearch/data/common/network/di/HandleRecoverablePlatformException.jvmCommon.kt
@@ -6,6 +6,7 @@ import ly.david.musicsearch.shared.domain.error.HandledException
 actual fun handleRecoverablePlatformException(exception: Throwable) {
     when (exception) {
         is java.net.ConnectException,
+        is java.net.UnknownHostException,
         is java.net.SocketTimeoutException,
         -> {
             throw HandledException("Network error. Check your internet connection.", ErrorResolution.Retry)
