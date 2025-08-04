73b0c952b38b0270686bbfadeee91d7a9e62b5a6
David Ly <22302001+lydavid@users.noreply.github.com>
2025-08-04 08:06:56 -0400

    feat: show the display language of aliases in details and relocate the locale so that it doesn't get cut off by a long alias name

--- shared/feature/details/src/commonMain/kotlin/ly/david/musicsearch/shared/feature/details/alias/AliasListItem.kt
+++ shared/feature/details/src/commonMain/kotlin/ly/david/musicsearch/shared/feature/details/alias/AliasListItem.kt
@@ -2,14 +2,11 @@ package ly.david.musicsearch.shared.feature.details.alias
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalClipboard
@@ -21,11 +18,13 @@ import kotlinx.coroutines.launch
 import ly.david.musicsearch.shared.domain.alias.AliasType
 import ly.david.musicsearch.shared.domain.alias.BasicAlias
 import ly.david.musicsearch.shared.domain.common.ifNotEmpty
+import ly.david.musicsearch.shared.domain.common.transformThisIfNotNullOrEmpty
 import ly.david.musicsearch.shared.domain.getLifeSpanForDisplay
 import ly.david.musicsearch.ui.common.clipboard.clipEntryWith
 import ly.david.musicsearch.ui.common.preview.PreviewTheme
 import ly.david.musicsearch.ui.common.theme.LocalStrings
 import ly.david.musicsearch.ui.common.theme.TextStyles
+import ly.david.musicsearch.ui.common.work.getDisplayLanguage
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -40,45 +39,39 @@ fun AliasListItem(
 
     ListItem(
         headlineContent = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Column {
-                    Text(
-                        text = alias.name,
-                        style = TextStyles.getCardBodyTextStyle(),
-                    )
-                    val typeAndLifeSpan = listOfNotNull(
-                        alias.type?.getDisplayString(strings),
-                        alias.getLifeSpanForDisplay().takeIf { it.isNotEmpty() },
-                        strings.primary.takeIf { alias.isPrimary },
-                    ).joinToString("・")
-                    typeAndLifeSpan.ifNotEmpty { typeAndLifeSpan ->
-                        Text(
-                            text = buildAnnotatedString {
-                                append(typeAndLifeSpan)
-                                if (alias.isPrimary && strings.primary.isNotEmpty()) {
-                                    val primaryStart = typeAndLifeSpan.lastIndexOf(strings.primary)
-                                    if (primaryStart >= 0) {
-                                        addStyle(
-                                            SpanStyle(fontWeight = FontWeight.SemiBold),
-                                            start = primaryStart,
-                                            end = primaryStart + strings.primary.length,
-                                        )
-                                    }
-                                }
-                            },
-                            style = TextStyles.getCardBodySubTextStyle(),
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.weight(1f))
-
+            Column {
                 Text(
-                    text = alias.locale,
+                    text = alias.name,
                     style = TextStyles.getCardBodyTextStyle(),
                 )
+
+                val locale = alias.locale
+                val displayLanguage = locale.getDisplayLanguage(strings)
+                    .transformThisIfNotNullOrEmpty { "$it ($locale)" }
+                val typeAndLifeSpan = listOfNotNull(
+                    alias.type?.getDisplayString(strings),
+                    alias.getLifeSpanForDisplay().takeIf { it.isNotEmpty() },
+                    displayLanguage.takeIf { it.isNotEmpty() },
+                    strings.primary.takeIf { alias.isPrimary },
+                ).joinToString("・")
+                typeAndLifeSpan.ifNotEmpty { typeAndLifeSpan ->
+                    Text(
+                        text = buildAnnotatedString {
+                            append(typeAndLifeSpan)
+                            if (alias.isPrimary && strings.primary.isNotEmpty()) {
+                                val primaryStart = typeAndLifeSpan.lastIndexOf(strings.primary)
+                                if (primaryStart >= 0) {
+                                    addStyle(
+                                        SpanStyle(fontWeight = FontWeight.SemiBold),
+                                        start = primaryStart,
+                                        end = primaryStart + strings.primary.length,
+                                    )
+                                }
+                            }
+                        },
+                        style = TextStyles.getCardBodySubTextStyle(),
+                    )
+                }
             }
         },
         modifier = modifier
@@ -98,10 +91,10 @@ internal fun PreviewAliasListItemPrimary() {
         Surface {
             AliasListItem(
                 alias = BasicAlias(
-                    name = "Comic Market 104",
+                    name = "The Apothecary Diaries Season 2 Volume 2 (Original Anime Soundtrack)",
                     locale = "en",
                     isPrimary = true,
-                    type = AliasType.EVENT_NAME,
+                    type = AliasType.RELEASE_GROUP_NAME,
                     begin = "",
                     end = "",
                     ended = false,
