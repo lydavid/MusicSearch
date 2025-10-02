8e29a45126c280ac01914d8a62259ba871ee264f
David Ly <22302001+lydavid@users.noreply.github.com>
2025-10-02 19:16:43 -0400

    fix: support scrolling listen's additional actions bottom sheet

--- shared/feature/listens/src/commonMain/kotlin/ly/david/musicsearch/shared/feature/listens/ListensUi.kt
+++ shared/feature/listens/src/commonMain/kotlin/ly/david/musicsearch/shared/feature/listens/ListensUi.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -370,7 +372,9 @@ internal fun ListenAdditionalActionsBottomSheetContent(
     onDismiss: () -> Unit = {},
 ) {
     val release = listen.release
-    Column {
+    Column(
+        modifier = Modifier.verticalScroll(state = rememberScrollState()),
+    ) {
         Row(
             modifier = Modifier.padding(horizontal = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
