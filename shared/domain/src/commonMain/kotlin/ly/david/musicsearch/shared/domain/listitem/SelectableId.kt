869edb7ee430d8f1b5b331fe7b3dad8db903b039
David Ly <22302001+lydavid@users.noreply.github.com>
2026-03-25 19:00:08 -0400

    fix: fix crash when app is put in background while a release screen is part of the backstack (Parcel IllegalArgumentException)

--- shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/listitem/SelectableId.kt
+++ shared/domain/src/commonMain/kotlin/ly/david/musicsearch/shared/domain/listitem/SelectableId.kt
@@ -1,10 +1,14 @@
 package ly.david.musicsearch.shared.domain.listitem
 
+import ly.david.musicsearch.shared.domain.parcelize.CommonParcelable
+import ly.david.musicsearch.shared.domain.parcelize.Parcelize
+
 /**
  * @param id Should be the unique [EntityListItemModel.id].
  * @param recordingId For [TrackListItemModel].
  */
+@Parcelize
 data class SelectableId(
     val id: String,
     val recordingId: String? = null,
-)
+) : CommonParcelable
